e3e6f1e11b8d53378dfc02205a6398fdfb0a93da
Lee Yarwood <lyarwood@redhat.com>
2022-01-27 15:20:12 +0000

    libvirt: Introduce support for qcow2 with LUKS
    This includes support for resize image and snapshot image in addition to
    create image.
    
    A new unit test test_driver_capabilities_flat is also added ahead of
    support for the RAW image backend. The Flat image backend was the only
    backend that did not have a driver capabilities unit test.
    
    TODOs
    
    - Adding support to the raw backend for luks
    
    This is going to take converting the initial RAW base into RAW LUKS.
    
    - Adding support to the rbd backend for luks
    
    https://docs.ceph.com/en/latest/rbd/rbd-encryption/
    
    - Deprecating the existing ephemeral encryption implementation
    - Tests, as you can see most of the stack doesn't have tests.
    - Documentation, lots of docs.
    
    Change-Id: I35cbc7b3f314605e256fb76d4be1ff1a109d50a3

--- nova/tests/unit/virt/libvirt/test_driver.py
+++ nova/tests/unit/virt/libvirt/test_driver.py
@@ -817,12 +817,12 @@ class LibvirtConnTestCase(test.NoDBTestCase,
             "Driver capabilities for 'supports_socket_pci_numa_affinity' "
             "is invalid",
         )
-        self.assertFalse(
+        self.assertTrue(
             drvr.capabilities['supports_ephemeral_encryption'],
             "Driver capabilities for 'supports_ephemeral_encryption' "
             "is invalid",
         )
-        self.assertFalse(
+        self.assertTrue(
             drvr.capabilities['supports_ephemeral_encryption_luks'],
             "Driver capabilities for 'supports_ephemeral_encryption_luks' "
             " is invalid",
@@ -908,6 +908,19 @@ class LibvirtConnTestCase(test.NoDBTestCase,
             "is invalid when host should support this feature"
         )
         mock_supports.assert_called_once_with()
+
+    def test_driver_capabilities_flat(self):
+        self.flags(use_cow_images=False)
+        drvr = libvirt_driver.LibvirtDriver(fake.FakeVirtAPI(), False)
+        self.assertFalse(
+            drvr.capabilities['supports_ephemeral_encryption'],
+            "Driver capabilities for 'supports_ephemeral_encryption' "
+            "is invalid")
+        self.assertFalse(
+            drvr.capabilities['supports_ephemeral_encryption_luks'],
+            "Driver capabilities for 'supports_ephemeral_encryption_luks' "
+            "is invalid",
+        )
 
     def test_driver_raises_on_non_linux_platform(self):
         with utils.temporary_mutation(sys, platform='darwin'):
@@ -14166,6 +14179,7 @@ class LibvirtConnTestCase(test.NoDBTestCase,
             'qcow2',
             virt_disk_size,
             backing_file=backfile_path,
+            encryption=None,
         )
 
     @mock.patch('nova.virt.libvirt.imagebackend.Image.exists',
@@ -14316,12 +14330,14 @@ class LibvirtConnTestCase(test.NoDBTestCase,
                     'qcow2',
                     disk_info_byname['disk']['virt_disk_size'],
                     backing_file=root_backing,
+                    encryption=None,
                 ),
                 mock.call(
                     CONF.instances_path + '/disk.local',
                     'qcow2',
                     disk_info_byname['disk.local']['virt_disk_size'],
                     backing_file=ephemeral_backing,
+                    encryption=None,
                 ),
             ])
 
