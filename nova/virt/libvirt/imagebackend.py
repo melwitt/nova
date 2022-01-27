e541903336abde7b367dc834ec3487bc5cc91cba
Lee Yarwood <lyarwood@redhat.com>
2022-01-27 15:20:12 +0000

    libvirt: Introduce support for qcow2 with LUKS
    TODOs
    
    - Adding support to the raw backend for luks
    
    This is going to take converting the initial RAW base into RAW LUKS.
    
    - Adding support to the rbd backend for luks
    
    https://docs.ceph.com/en/latest/rbd/rbd-encryption/
    
    - Deprecating the existing ephemeral encryption implementation
    - Tests, as you can see most of the stack doesn't have tests.
    - Documentation, lots of docs.
    
    Change-Id: I35cbc7b3f314605e256fb76d4be1ff1a109d50a3

--- nova/virt/libvirt/imagebackend.py
+++ nova/virt/libvirt/imagebackend.py
@@ -649,6 +649,9 @@ class Flat(Image):
 
 
 class Qcow2(Image):
+
+    SUPPORTS_LUKS = True
+
     def __init__(
         self, instance=None, disk_name=None, path=None, disk_info_mapping=None
     ):
@@ -670,9 +673,10 @@ class Qcow2(Image):
         filename = self._get_lock_name(base)
 
         @utils.synchronized(filename, external=True, lock_path=self.lock_path)
-        def create_qcow2_image(base, target, size):
+        def create_qcow2_image(base, target, size, encryption):
             libvirt_utils.create_image(
-                target, 'qcow2', size, backing_file=base)
+                target, 'qcow2', size, backing_file=base,
+                encryption=encryption)
 
         # Download the unmodified base image unless we already have a copy.
         if not os.path.exists(base):
@@ -709,9 +713,22 @@ class Qcow2(Image):
                                                     imgmodel.FORMAT_QCOW2)
                     disk.extend(image, legacy_backing_size)
 
+        encryption = None
+        if self.disk_info_mapping and self.disk_info_mapping.get('encrypted'):
+            # FIXME(lyarwood): Context is provided as a kwarg here thanks to
+            # the legacy ephemeral encryption implementation. It should likely
+            # be an arg but the required refactor isn't trivial.
+            context = kwargs['context']
+            secret_uuid = self.disk_info_mapping.get('encryption_secret_uuid')
+            secret = self.key_manager.get(context, secret_uuid).get_encoded()
+            encryption = {
+                'format': self.disk_info_mapping.get('encryption_format'),
+                'secret': secret,
+            }
+
         if not os.path.exists(self.path):
             with fileutils.remove_path_on_error(self.path):
-                create_qcow2_image(base, self.path, size)
+                create_qcow2_image(base, self.path, size, encryption)
 
     def resize_image(self, size):
         image = imgmodel.LocalFileImage(self.path, imgmodel.FORMAT_QCOW2)
