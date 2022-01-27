0f14ec3b2f5413084a32818dae4c8fa32ffa7041
Lee Yarwood <lyarwood@redhat.com>
2022-01-27 15:08:16 +0000

    libvirt: Configure and teardown ephemeral encryption secrets
    This adds configuration of the default ephemeral encryption format and
    sets default encryption attributes in the driver block device mapping
    when needed. This includes generation of a secret passphrase when one
    has not been provided.
    
    Co-Authored-By: melanie witt <melwittt@gmail.com>
    
    Related to blueprint ephemeral-encryption-libvirt
    
    Change-Id: I052441076c677c0fe76a8d9421af70b0ffa1d400

--- nova/exception.py
+++ nova/exception.py
@@ -2546,3 +2546,8 @@ class NotSupportedComputeForEvacuateV295(NotSupported):
                 "instance on destination. To evacuate before upgrades are "
                 "complete please use an older microversion. Required version "
                 "for compute %(expected), current version %(currently)s")
+
+
+class EphemeralEncryptionSecretNotFound(Invalid):
+    msg_fmt = _(
+        'Encryption secret %(secret_uuid)s was not found in the key manager')
