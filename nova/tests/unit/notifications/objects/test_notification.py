30a85173254c9e1ba92039f1e7066327fc9f27b6
melanie witt <melwittt@gmail.com>
2022-12-09 22:57:43 +0000

    Add hw_ephemeral_encryption_secret_uuid image property
    If an image is encrypted, we will need to retrieve the passphrase from
    the key manager service in order to create an instance from it.
    
    This adds an image property to store the secret UUID that belongs to
    the image. It will only be used to decrypt the image and will not be
    used to encrypt or decrypt any other image. Nova will create a new
    secret for each disk image it creates, including snapshots.
    
    Related to blueprint ephemeral-storage-encryption
    
    Change-Id: I01eef6adc2c8feb64e86b33392b8b4b483041e27

--- nova/tests/unit/notifications/objects/test_notification.py
+++ nova/tests/unit/notifications/objects/test_notification.py
@@ -386,7 +386,7 @@ notification_object_data = {
     # ImageMetaProps, so when you see a fail here for that reason, you must
     # *also* bump the version of ImageMetaPropsPayload. See its docstring for
     # more information.
-    'ImageMetaPropsPayload': '1.13-24345c28a6463e85e12902d43af0ecf2',
+    'ImageMetaPropsPayload': '1.14-588eb78f4f2028450b380f1a2204b5e1',
     'InstanceActionNotification': '1.0-a73147b93b520ff0061865849d3dfa56',
     'InstanceActionPayload': '1.8-4fa3da9cbf0761f1f700ae578f36dc2f',
     'InstanceActionRebuildNotification':
