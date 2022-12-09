7b78482f636a1861e4666dd0cab898fd66e00dbe
melanie witt <melwittt@gmail.com>
2022-12-09 22:57:43 +0000

    Add hw_ephemeral_encryption_secret_uuid image property
    If an image is encrypted, we will need to retrieve the passphrase from
    the key manager service in order to create an instance from it.
    
    This adds an image property to store the secret UUID that belongs to
    the image.
    
    Change-Id: I01eef6adc2c8feb64e86b33392b8b4b483041e27

--- nova/objects/image_meta.py
+++ nova/objects/image_meta.py
@@ -192,14 +192,17 @@ class ImageMetaProps(base.NovaObject):
     #                     'hw_ephemeral_encryption_format' fields
     # Version 1.33: Added 'hw_locked_memory' field
     # Version 1.34: Added 'hw_viommu_model' field
+    # Version 1.35: Added 'hw_ephemeral_encryption_secret_uuid' field
     # NOTE(efried): When bumping this version, the version of
     # ImageMetaPropsPayload must also be bumped. See its docstring for details.
-    VERSION = '1.34'
+    VERSION = '1.35'
 
     def obj_make_compatible(self, primitive, target_version):
         super(ImageMetaProps, self).obj_make_compatible(primitive,
                                                         target_version)
         target_version = versionutils.convert_version_to_tuple(target_version)
+        if target_version < (1, 35):
+            primitive.pop('hw_ephemeral_encryption_secret_uuid', None)
         if target_version < (1, 34):
             primitive.pop('hw_viommu_model', None)
         if target_version < (1, 33):
@@ -472,6 +475,8 @@ class ImageMetaProps(base.NovaObject):
         # encryption format to be used when ephemeral encryption is enabled
         'hw_ephemeral_encryption_format':
             fields.BlockDeviceEncryptionFormatTypeField(),
+        # encryption secret uuid string for passphrase in the image
+        'hw_ephemeral_encryption_secret_uuid': fields.UUIDField(),
 
         # if true download using bittorrent
         'img_bittorrent': fields.FlexibleBooleanField(),
