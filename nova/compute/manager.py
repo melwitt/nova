e3a5cf7731d35e8535170664a9f1b2009ee518c3
melanie witt <melwittt@gmail.com>
2023-01-12 23:50:56 +0000

    Update driver BDMs with ephemeral encryption image properties
    This comes up when doing a rebuild or a shelve/unshelve.
    
    The "old" encryption secret UUID remains stored in the driver BDMs but
    when we use a different image, we need the driver BDMs to reflect the
    new corresponding encryption secret UUID.
    
    Related to blueprint ephemeral-storage-encryption
    
    Change-Id: I10e2328dfe70216b822569becfd21976152293a1
    
    support shelve/unshelve
    
    Change-Id: I09113512f9e2cec442ce61ef145a8ea170cf319c

--- nova/compute/manager.py
+++ nova/compute/manager.py
@@ -3588,6 +3588,15 @@ class ComputeManager(manager.Manager):
                     self._rebuild_volume_backed_instance(
                         context, instance, bdms, image_meta.id)
 
+            # We are potentially changing images, so wipe out the old
+            # encryption attributes in the BDMs and update them with properties
+            # from the new image. We could be going from not encrypted to
+            # encrypted, for example. Note that we have to do this after
+            # calling driver.destroy() because we needed the old encryption
+            # secret UUID in the BDM in order to delete the old secret.
+            self.reset_and_update_ephemeral_encryption_bdms(
+                instance.flavor, image_meta, bdms)
+
         instance.task_state = task_states.REBUILD_BLOCK_DEVICE_MAPPING
         instance.save(expected_task_state=[task_states.REBUILDING])
 
@@ -3603,6 +3612,14 @@ class ComputeManager(manager.Manager):
                               network_info=network_info,
                               block_device_info=new_block_device_info,
                               accel_info=accel_info)
+
+    @staticmethod
+    def reset_and_update_ephemeral_encryption_bdms(flavor, image_meta, bdms):
+        if bdms:
+            bdms.reset_encryption_fields()
+            compute_utils.update_ephemeral_encryption_bdms(
+                flavor, image_meta, bdms)
+            bdms.save_all()
 
     def _notify_instance_rebuild_error(self, context, instance, error, bdms):
         self._notify_about_instance_usage(context, instance,
@@ -6875,6 +6892,27 @@ class ComputeManager(manager.Manager):
         instance.task_state = task_states.SPAWNING
         instance.save()
 
+        if image:
+            image_meta = objects.ImageMeta.from_dict(image)
+        else:
+            image_meta = objects.ImageMeta.from_dict(
+                utils.get_image_from_system_metadata(
+                    instance.system_metadata))
+
+        # Update the encryption secret UUID in system metadata with the UUID
+        # from the image, if there is one.
+        key = 'hw_ephemeral_encryption_secret_uuid'
+        if key in image_meta.properties:
+            instance.system_metadata['image_' + key] = (
+                image_meta.properties.get(key))
+
+        # Update encryption fields in the BDMs with values from the image
+        # properties before generating block_device_info. Example: the
+        # encryption secret UUID for the image will be different than the UUID
+        # of the previous image before the instance was shelved.
+        self.reset_and_update_ephemeral_encryption_bdms(
+            instance.flavor, image_meta, bdms)
+
         block_device_info = self._prep_block_device(context, instance, bdms)
         scrubbed_keys = self._unshelve_instance_key_scrub(instance)
 
@@ -6886,14 +6924,10 @@ class ComputeManager(manager.Manager):
         allocations = self.reportclient.get_allocations_for_consumer(
             context, instance.uuid)
 
+        # Save the original image_ref
         shelved_image_ref = instance.image_ref
         if image:
             instance.image_ref = image['id']
-            image_meta = objects.ImageMeta.from_dict(image)
-        else:
-            image_meta = objects.ImageMeta.from_dict(
-                utils.get_image_from_system_metadata(
-                    instance.system_metadata))
 
         provider_mappings = self._get_request_group_mapping(request_spec)
 
