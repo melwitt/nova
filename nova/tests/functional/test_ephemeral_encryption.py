36fa1b740b469390614ac1fa9eae56df4f7f4263
melanie witt <melwittt@gmail.com>
2023-01-12 23:50:56 +0000

    Support rebuild with ephemeral encryption for qcow2
    For rebuilds with a new image, the image properties are validated
    against the instance's flavor to ensure there are no conflicts, else a
    HTTP 400 is returned.
    
    Rebuilds with a new image are allowed to change the instance from using
    ephemeral encryption to not using ephemeral encryption via the image
    properties, as long as the rebuild request is being made by the user_id
    that owns the instance. This is meant to ensure encryption secrets are
    always accessible to owner of the instance. Requests from different
    users are rejected with HTTP 403.
    
    Ephemeral encryption attributes in the BDMs are reset during a rebuild
    from requesting ephemeral encryption to no ephemeral encryption.
    
    Related to blueprint ephemeral-storage-encryption
    
    Change-Id: I09113512f9e2cec442ce61ef145a8ea170cf319c

--- nova/tests/functional/test_ephemeral_encryption.py
+++ nova/tests/functional/test_ephemeral_encryption.py
@@ -10,6 +10,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import ddt
 from oslo_utils.fixture import uuidsentinel
 
 from nova import context
@@ -531,3 +532,111 @@ class TestEphemeralEncryptionResize(_TestEphemeralEncryptionBase):
             api_client.OpenStackApiException, self._resize_server, server,
             uuidsentinel.eph_encryption_plain_flavor)
         self.assertEqual(400, ex.response.status_code)
+
+
+@ddt.ddt
+class TestEphemeralEncryptionRebuild(_TestEphemeralEncryptionBase):
+
+    compute_driver = 'fake.EphEncryptionDriverLUKSPLAIN'
+    flavors = {
+        'no_eph_encryption': {
+            'id': uuidsentinel.no_eph_encryption
+        },
+        'eph_encryption': {
+            'id': uuidsentinel.eph_encryption_flavor,
+            'extra_specs': {
+                'hw:ephemeral_encryption': 'True'
+            }
+        },
+        'eph_encryption_disabled': {
+            'id': uuidsentinel.eph_encryption_disabled_flavor,
+            'extra_specs': {
+                'hw:ephemeral_encryption': 'False'
+            }
+        },
+        'eph_encryption_luks': {
+            'id': uuidsentinel.eph_encryption_luks_flavor,
+            'extra_specs': {
+                'hw:ephemeral_encryption': 'True',
+                'hw:ephemeral_encryption_format': 'luks'
+            }
+        },
+        'eph_encryption_plain': {
+            'id': uuidsentinel.eph_encryption_plain_flavor,
+            'extra_specs': {
+                'hw:ephemeral_encryption': 'True',
+                'hw:ephemeral_encryption_format': 'plain'
+            }
+        },
+    }
+
+    @ddt.data(
+        (uuidsentinel.eph_encryption_flavor,
+         {'hw_ephemeral_encryption': 'False'}),
+        (uuidsentinel.eph_encryption_luks_flavor,
+         {'hw_ephemeral_encryption': 'False'}),
+        (uuidsentinel.eph_encryption_plain_flavor,
+         {'hw_ephemeral_encryption': 'False'}),
+        (uuidsentinel.eph_encryption_disabled_flavor,
+         {'hw_ephemeral_encryption': 'True'}),
+        (uuidsentinel.eph_encryption_disabled_flavor,
+         {'hw_ephemeral_encryption': 'True',
+          'hw_ephemeral_encryption_format': 'luks'}),
+        (uuidsentinel.eph_encryption_disabled_flavor,
+         {'hw_ephemeral_encryption': 'True',
+          'hw_ephemeral_encryption_format': 'plain'}),
+        (uuidsentinel.eph_encryption_luks_flavor,
+         {'hw_ephemeral_encryption': 'True',
+          'hw_ephemeral_encryption_format': 'plain'}),
+        (uuidsentinel.eph_encryption_plain_flavor,
+         {'hw_ephemeral_encryption': 'True',
+          'hw_ephemeral_encryption_format': 'luks'}))
+    @ddt.unpack
+    def test_flavor_image_mismatch(self, flavor_id, image_meta):
+        # Test a scenario where a rebuild with a new image is requested and the
+        # image property conflicts with the instance's flavor extra specs.
+        server = self._create_server(flavor_id=flavor_id, networks=[])
+        image = self._create_image(image_meta)
+        ex = self.assertRaises(
+            api_client.OpenStackApiException, self._rebuild_server, server,
+            image['id'])
+        self.assertEqual(400, ex.response.status_code)
+
+    @ddt.data(
+        (uuidsentinel.eph_encryption_flavor,
+         {'hw_ephemeral_encryption': 'True'}),
+        (uuidsentinel.no_eph_encryption,
+         {'hw_ephemeral_encryption': 'True'}),
+        (uuidsentinel.eph_encryption_luks_flavor,
+         {'hw_ephemeral_encryption': 'True'}),
+        (uuidsentinel.eph_encryption_plain_flavor,
+         {'hw_ephemeral_encryption': 'True'}),
+        (uuidsentinel.no_eph_encryption,
+         {'hw_ephemeral_encryption': 'False'}),
+        (uuidsentinel.eph_encryption_disabled_flavor,
+         {'hw_ephemeral_encryption': 'False'}),
+        (uuidsentinel.eph_encryption_luks_flavor,
+         {'hw_ephemeral_encryption': 'True',
+          'hw_ephemeral_encryption_format': 'luks'}),
+        (uuidsentinel.eph_encryption_plain_flavor,
+         {'hw_ephemeral_encryption': 'True',
+          'hw_ephemeral_encryption_format': 'plain'}),
+        (uuidsentinel.no_eph_encryption,
+         {'hw_ephemeral_encryption': 'True',
+          'hw_ephemeral_encryption_format': 'luks'}),
+        (uuidsentinel.no_eph_encryption,
+         {'hw_ephemeral_encryption': 'True',
+          'hw_ephemeral_encryption_format': 'plain'}),
+        (uuidsentinel.eph_encryption_flavor, {}),
+        (uuidsentinel.no_eph_encryption, {}),
+        (uuidsentinel.eph_encryption_disabled_flavor, {}),
+        (uuidsentinel.eph_encryption_luks_flavor, {}),
+        (uuidsentinel.eph_encryption_plain_flavor, {}))
+    @ddt.unpack
+    def test_flavor_image_matches(self, flavor_id, image_meta):
+        # Test a scenario where a rebuild with a new image is requested and the
+        # image property matches with the instance's flavor extra specs.
+        server = self._create_server(flavor_id=flavor_id, networks=[])
+        image = self._create_image(image_meta)
+        # Rebuild should not be rejected.
+        self._rebuild_server(server, image['id'])
