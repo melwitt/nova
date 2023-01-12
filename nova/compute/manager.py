6db448d58bf13d14c2d82c60592d271c1ab2d70e
melanie witt <melwittt@gmail.com>
2023-01-12 23:50:56 +0000

    Update driver BDMs with ephemeral encryption image properties
    This comes up when doing a rebuild or a shelve/unshelve.
    
    The "old" encryption secret UUID remains stored in the driver BDMs but
    when we use a different image, we need the driver BDMs to reflect the
    new corresponding encryption secret UUID.
    
    Related to blueprint ephemeral-storage-encryption
    
    Change-Id: I10e2328dfe70216b822569becfd21976152293a1
    
    support shelve/unshelve
    
    Change-Id: I09113512f9e2cec442ce61ef145a8ea170cf319c

--- nova/compute/manager.py
+++ nova/compute/manager.py
@@ -3658,11 +3658,12 @@ class ComputeManager(manager.Manager):
                 ]
                 self.driver.destroy(context, instance,
                                     network_info=network_info,
-                                    block_device_info=block_device_info_copy)
+                                    block_device_info=block_device_info_copy,
+                                    destroy_secrets=False)
             else:
                 self.driver.destroy(context, instance,
                     network_info=network_info,
-                    block_device_info=block_device_info)
+                    block_device_info=block_device_info, destroy_secrets=False)
             try:
                 accel_info = self._get_accel_info(context, instance)
             except Exception as exc:
@@ -3677,6 +3678,14 @@ class ComputeManager(manager.Manager):
                 if is_volume_backed:
                     self._rebuild_volume_backed_instance(
                         context, instance, bdms, image_meta.id)
+
+            # We are potentially changing images, so wipe out the old
+            # encryption attributes in the BDMs and update them with properties
+            # from the new image. We could be going from not encrypted to
+            # encrypted, for example. Note that we have to do this after
+            # calling driver.destroy() because we needed the old encryption
+            # secret UUID in the BDM in order to delete the old secret.
+            self.reset_ephemeral_encryption_image_sysmeta(instance, image_meta)
 
         instance.task_state = task_states.REBUILD_BLOCK_DEVICE_MAPPING
         instance.save(expected_task_state=[task_states.REBUILDING])
@@ -3693,6 +3702,54 @@ class ComputeManager(manager.Manager):
                               network_info=network_info,
                               block_device_info=new_block_device_info,
                               accel_info=accel_info)
+
+    @staticmethod
+    def reset_ephemeral_encryption_image_sysmeta(
+        instance: 'objects.Instance',
+        image_meta: 'objects.ImageMeta',
+    ) -> None:
+        """Reset ephemeral encryption attributes in instance sys meta
+
+        We will need the encryption secret and format in the virt driver in
+        order to do things like convert and rebase images. The virt driver will
+        pull them from the block device mappings and instance system metadata.
+
+        The image properties in the system metadata need to be updated
+        if the instance is being booted with a different image than the one
+        with which it was first booted. Examples are rebuild and
+        shelve/unshelve. When an instance is shelved, a snapshot is taken and
+        after it is offloaded, the virt guest is destroyed and resources are
+        freed.
+
+        When we snapshot an encrypted local disk, we save the encryption secret
+        UUID in the image properties of the uploaded snapshot image.
+
+        Later, when we need to unshelve the instance, we will boot a new virt
+        guest from the snapshot image and from then on, the instance will need
+        the encryption secret UUID for that image in order to do potential
+        image converts or rebases in the future.
+        """
+        # image_meta can be an empty objects.ImageMeta() (example: rebuild
+        # without changing the image)
+        if 'properties' not in image_meta:
+            return
+
+        sysmeta = instance.system_metadata
+        prefix = utils.SM_IMAGE_PROP_PREFIX
+
+        need_save = False
+        for key in (
+            'hw_ephemeral_encryption',
+            'hw_ephemeral_encryption_secret_uuid',
+            'hw_ephemeral_encryption_format'
+        ):
+            value = image_meta.properties.get(key)
+            if value:
+                sysmeta[prefix + key] = value
+                need_save = True
+
+        if need_save:
+            instance.save()
 
     def _notify_instance_rebuild_error(self, context, instance, error, bdms):
         self._notify_about_instance_usage(context, instance,
@@ -6878,7 +6935,7 @@ class ComputeManager(manager.Manager):
                                                                  instance,
                                                                  bdms=bdms)
         self.driver.destroy(context, instance, network_info,
-                block_device_info)
+                block_device_info, destroy_secrets=False)
 
         # the instance is going to be removed from the host so we want to
         # terminate all the connections with the volume server and the host
@@ -6980,6 +7037,19 @@ class ComputeManager(manager.Manager):
         instance.task_state = task_states.SPAWNING
         instance.save()
 
+        if image:
+            image_meta = objects.ImageMeta.from_dict(image)
+        else:
+            image_meta = objects.ImageMeta.from_dict(
+                utils.get_image_from_system_metadata(
+                    instance.system_metadata))
+
+        # Update encryption fields in the BDMs with values from the image
+        # properties before generating block_device_info. Example: the
+        # encryption secret UUID for the image will be different than the UUID
+        # of the previous image before the instance was shelved.
+        self.reset_ephemeral_encryption_image_sysmeta(instance, image_meta)
+
         block_device_info = self._prep_block_device(context, instance, bdms)
         scrubbed_keys = self._unshelve_instance_key_scrub(instance)
 
@@ -6991,14 +7061,10 @@ class ComputeManager(manager.Manager):
         allocations = self.reportclient.get_allocations_for_consumer(
             context, instance.uuid)
 
+        # Save the original image_ref
         shelved_image_ref = instance.image_ref
         if image:
             instance.image_ref = image['id']
-            image_meta = objects.ImageMeta.from_dict(image)
-        else:
-            image_meta = objects.ImageMeta.from_dict(
-                utils.get_image_from_system_metadata(
-                    instance.system_metadata))
 
         provider_mappings = self._get_request_group_mapping(request_spec)
 
