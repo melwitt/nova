0c6be8bc888a5a017331cebb7199758f091164c7
melanie witt <melwittt@gmail.com>
2023-01-12 23:50:56 +0000

    Support rebuild and unshelve with ephemeral encryption
    When an instance is created with ephemeral encryption specified, the
    image properties related to the encryption (if present) are taken from
    the image and then stored in the instance system metadata. This
    includes the secret UUID needed to read the image.
    
    When an instance is snaphotted or shelved, the created image will have
    a new secret and its UUID will be kept in the created image's image
    properties.
    
    If/when the encrypted snapshot image is used later to create a new
    instance or unshelve an existing instance, the new image's image
    properties including its secret UUID will be needed to create or
    unshelve the instance.
    
    This enables ephemeral encryption related image properties to be
    refreshed in the instance system metadata when a new image is used
    during a rebuild or unshelve.
    
    Related to blueprint ephemeral-storage-encryption
    
    Change-Id: I09113512f9e2cec442ce61ef145a8ea170cf319c

--- nova/virt/vmwareapi/driver.py
+++ nova/virt/vmwareapi/driver.py
@@ -225,7 +225,7 @@ class VMwareVCDriver(driver.ComputeDriver):
 
     def cleanup(self, context, instance, network_info, block_device_info=None,
                 destroy_disks=True, migrate_data=None, destroy_vifs=True,
-                destroy_secrets=True):
+                destroy_secrets=True, destroy_ephemeral_secrets=True):
         """Cleanup after instance being destroyed by Hypervisor."""
         pass
 
@@ -598,7 +598,8 @@ class VMwareVCDriver(driver.ComputeDriver):
                                   instance=instance)
 
     def destroy(self, context, instance, network_info, block_device_info=None,
-                destroy_disks=True, destroy_secrets=True):
+                destroy_disks=True, destroy_secrets=True,
+                destroy_ephemeral_secrets=True):
         """Destroy VM instance."""
 
         # Destroy gets triggered when Resource Claim in resource_tracker
