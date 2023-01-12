1240ff5a79f203068a31c450913bf77613750e65
melanie witt <melwittt@gmail.com>
2023-01-12 23:50:56 +0000

    Support rebuild and unshelve with ephemeral encryption
    When an instance is created with ephemeral encryption specified, the
    image properties related to the encryption (if present) are taken from
    the image and then stored in the instance system metadata. This
    includes the secret UUID needed to read the image.
    
    When an instance is snaphotted or shelved, the created image will have
    a new secret and its UUID will be kept in the created image's image
    properties.
    
    If/when the encrypted snapshot image is used later to create a new
    instance or unshelve an existing instance, the new image's image
    properties including its secret UUID will be needed to create or
    unshelve the instance.
    
    This enables ephemeral encryption related image properties to be
    refreshed in the instance system metadata when a new image is used
    during a rebuild or unshelve.
    
    Related to blueprint ephemeral-storage-encryption
    
    Change-Id: I09113512f9e2cec442ce61ef145a8ea170cf319c

--- nova/compute/api.py
+++ nova/compute/api.py
@@ -3611,7 +3611,6 @@ class API:
             if img_arch:
                 fields_obj.Architecture.canonicalize(img_arch)
 
-    @reject_ephemeral_encryption_instances(instance_actions.REBUILD)
     @reject_vtpm_instances(instance_actions.REBUILD)
     @block_accelerators(until_service=SUPPORT_ACCELERATOR_SERVICE_FOR_REBUILD)
     # TODO(stephenfin): We should expand kwargs out to named args
@@ -3732,6 +3731,9 @@ class API:
         # numa constraints.
         if orig_image_ref != image_href:
             self._validate_numa_rebuild(instance, image, flavor)
+
+        if orig_image_ref != image_href:
+            self._validate_rebuild_for_ephemeral_encryption(flavor, image)
 
         kernel_id, ramdisk_id = self._handle_kernel_and_ramdisk(
                 context, None, None, image)
@@ -4216,6 +4218,34 @@ class API:
                         'not allowed.')
                     raise exception.EphemeralEncryptionConflict(
                         action='resize', reason=reason)
+
+    @staticmethod
+    def _validate_rebuild_for_ephemeral_encryption(flavor, image):
+        image_meta = objects.ImageMeta.from_dict(image)
+        # This will validate hw:ephemeral_encryption and
+        # hw_ephemeral_encryption.
+        hardware.get_ephemeral_encryption_constraint(flavor, image_meta)
+        # This will validate hw:ephemeral_encryption_format and
+        # hw_ephemeral_encryption_format.
+        flavor_format = flavor.get('extra_specs', {}).get(
+            'hw:ephemeral_encryption_format')
+        image_format = image_meta.properties.get(
+            'hw_ephemeral_encryption_format')
+        if (flavor_format is not None and image_format is not None and
+                flavor_format != image_format):
+            emsg = _(
+                'Flavor %(flavor_name)s has hw:ephemeral_encryption_format '
+                'extra spec explicitly set to %(flavor_val)s, conflicting '
+                'with image %(image_name)s which has '
+                'hw_ephemeral_encryption_format property explicitly set to '
+                '%(image_val)s')
+            data = {
+                'flavor_name': flavor.name,
+                'flavor_val': flavor_format,
+                'image_name': image_meta.name,
+                'image_val': image_format,
+            }
+            raise exception.FlavorImageConflict(emsg % data)
 
     # TODO(stephenfin): This logic would be so much easier to grok if we
     # finally split resize and cold migration into separate code paths
