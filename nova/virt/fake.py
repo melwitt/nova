b3d31f14190a62413e4a03836cc8b959d25e69f9
melanie witt <melwittt@gmail.com>
2023-01-16 06:37:08 +0000

    Support (resize|cold migration) with ephemeral encryption for qcow2
    Related to blueprint ephemeral-encryption-libvirt
    
    Change-Id: I76075fb9d6cc070af58da4950c096952fe5b4036

--- nova/virt/fake.py
+++ nova/virt/fake.py
@@ -1130,6 +1130,14 @@ class EphEncryptionDriverPLAIN(MediumFakeDriver):
         supports_ephemeral_encryption_plain=True)
 
 
+class EphEncryptionDriverLUKSPLAIN(MediumFakeDriver):
+    capabilities = dict(
+        FakeDriver.capabilities,
+        supports_ephemeral_encryption=True,
+        supports_ephemeral_encryption_luks=True,
+        supports_ephemeral_encryption_plain=True)
+
+
 class FakeDriverWithoutFakeNodes(FakeDriver):
     """FakeDriver that behaves like a real single-node driver.
 
