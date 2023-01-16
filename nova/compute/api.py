716a68fa9b287a066b4b68c5354f8daa043950cf
melanie witt <melwittt@gmail.com>
2023-01-16 06:37:08 +0000

    Support (resize|cold migration) with ephemeral encryption for qcow2
    Related to blueprint ephemeral-encryption-libvirt
    
    Change-Id: I76075fb9d6cc070af58da4950c096952fe5b4036

--- nova/compute/api.py
+++ nova/compute/api.py
@@ -4171,6 +4171,11 @@ class API:
         # is upgraded to the point of supporting cross-cell resize on all
         # compute services.
         if allowed:
+            # TODO(melwitt): Remove this block when snapshot with ephemeral
+            # encryption is supported.
+            if hardware.get_ephemeral_encryption_constraint(
+                    instance.flavor, instance.image_meta):
+                return False
             # TODO(mriedem): We can remove this minimum compute version check
             # in the 22.0.0 "V" release.
             min_compute_version = (
@@ -4242,7 +4247,44 @@ class API:
 
         return node
 
-    @reject_ephemeral_encryption_instances(instance_actions.RESIZE)
+    @staticmethod
+    def _validate_resize_for_ephemeral_encryption(
+            context, instance, current_flavor, new_flavor):
+        # Check that the new flavor does not conflict with the current image.
+        hardware.get_ephemeral_encryption_constraint(
+            new_flavor, instance.image_meta)
+        # Check if this is a request to resize from a flavor without
+        # ephemeral encryption to a flavor with ephemeral encryption and
+        # vice versa.
+        current_flavor_encryption = strutils.bool_from_string(
+            current_flavor.extra_specs.get('hw:ephemeral_encryption'))
+        new_flavor_encryption = strutils.bool_from_string(
+            new_flavor.extra_specs.get('hw:ephemeral_encryption'))
+        if current_flavor_encryption != new_flavor_encryption:
+            reason = _(
+                'Resize from a flavor with ephemeral encryption to a '
+                'flavor without ephemeral encryption and vice versa is not '
+                'allowed.')
+            raise exception.EphemeralEncryptionConflict(
+                action='resize', reason=reason)
+        # If both flavors specify encryption, check if the formats match.
+        if current_flavor_encryption and new_flavor_encryption:
+            current_format = current_flavor.extra_specs.get(
+                'hw:ephemeral_encryption_format')
+            new_format = new_flavor.extra_specs.get(
+                'hw:ephemeral_encryption_format')
+            if new_format is not None and current_format != new_format:
+                # Check BDM first before failing. If the BDM encryption format
+                # matches the new flavor format, we don't need to reject it.
+                bdms = objects.BlockDeviceMappingList.get_by_instance_uuid(
+                    context, instance.uuid)
+                if bdms.root_bdm().encryption_format != new_format:
+                    reason = _(
+                        'Resize to a different ephemeral encryption format is '
+                        'not allowed.')
+                    raise exception.EphemeralEncryptionConflict(
+                        action='resize', reason=reason)
+
     # TODO(stephenfin): This logic would be so much easier to grok if we
     # finally split resize and cold migration into separate code paths
     @block_extended_resource_request
@@ -4358,6 +4400,8 @@ class API:
                 self._validate_flavor_image_nostatus(
                     context, image, new_flavor, root_bdm=None,
                     validate_pci=True)
+            self._validate_resize_for_ephemeral_encryption(
+                context, instance, current_flavor, new_flavor)
 
         filter_properties = {'ignore_hosts': []}
         if not self._allow_resize_to_same_host(same_flavor, instance):
--- nova/tests/unit/virt/libvirt/test_driver.py
+++ nova/tests/unit/virt/libvirt/test_driver.py
@@ -735,7 +735,7 @@ class LibvirtConnTestCase(test.NoDBTestCase,
         os_vif.initialize()
 
         self.stub_out('nova.virt.disk.api.extend',
-                      lambda image, size, use_cow=False: None)
+                      lambda image, size, use_cow=False, encryption=None: None)
 
         self.stub_out('nova.virt.libvirt.imagebackend.Image.'
                       'resolve_driver_format',
@@ -14655,6 +14655,7 @@ class LibvirtConnTestCase(test.NoDBTestCase,
              '/fake/instance/dir/foo',
              disk_info['type'],
              disk_info['virt_disk_size'],
+             encryption=None,
         )
         mock_exists.assert_called_once_with('/fake/instance/dir/foo')
 
@@ -14763,7 +14764,8 @@ class LibvirtConnTestCase(test.NoDBTestCase,
                 mock.call(self.context, ramdisk_path, instance.ramdisk_id,
                           trusted_certs)
             ])
-            resize_image_mock.assert_called_once_with(virt_disk_size)
+            resize_image_mock.assert_called_once_with(
+                virt_disk_size, encryption=None)
 
         mock_utime.assert_called()
         mock_create_cow_image.assert_called_once_with(
@@ -15474,6 +15476,7 @@ class LibvirtConnTestCase(test.NoDBTestCase,
 
         def check_instance_dir(context, instance,
                                instance_dir, disk_info,
+                               block_device_info=None,
                                fallback_from_host=False):
             self.assertTrue(instance_dir)
         # creating mocks
@@ -15494,6 +15497,7 @@ class LibvirtConnTestCase(test.NoDBTestCase,
                                           migrate_data=migrate_data)
             create_image_mock.assert_has_calls(
                 [mock.call(self.context, instance, mock.ANY, {},
+                           block_device_info=None,
                            fallback_from_host=instance.host)])
             self.assertIsInstance(res, objects.LibvirtLiveMigrateData)
 
@@ -23976,7 +23980,7 @@ class LibvirtDriverTestCase(test.NoDBTestCase, TraitsComparisonMixin):
         self.drvr.confirm_migration(self.context, "migration_ref", ins_ref,
                                     _fake_network_info(self))
         mock_cleanup.assert_called_once_with(
-            self.context, ins_ref, _fake_network_info(self))
+            self.context, "migration_ref", ins_ref, _fake_network_info(self))
 
     @mock.patch('time.sleep', new=mock.Mock())
     def test_cleanup_resize_same_host(self):
@@ -23998,7 +24002,7 @@ class LibvirtDriverTestCase(test.NoDBTestCase, TraitsComparisonMixin):
             mock_get_path.return_value = '/fake/inst'
 
             drvr._cleanup_resize(
-                self.context, instance, _fake_network_info(self))
+                self.context, None, instance, _fake_network_info(self))
             mock_get_path.assert_called_once_with(instance)
             self.assertEqual(5, mock_rmtree.call_count)
 
@@ -24028,7 +24032,7 @@ class LibvirtDriverTestCase(test.NoDBTestCase, TraitsComparisonMixin):
             mock_exists.return_value = True
             mock_get_path.return_value = '/fake/inst'
 
-            drvr._cleanup_resize(self.context, instance, fake_net)
+            drvr._cleanup_resize(self.context, None, instance, fake_net)
 
             mock_get_path.assert_called_once_with(instance)
             self.assertEqual(5, mock_rmtree.call_count)
@@ -24065,7 +24069,7 @@ class LibvirtDriverTestCase(test.NoDBTestCase, TraitsComparisonMixin):
             error = exception.InternalError("fake error")
             mock_unplug.side_effect = error
 
-            drvr._cleanup_resize(self.context, instance, fake_net)
+            drvr._cleanup_resize(self.context, None, instance, fake_net)
 
             mock_get_path.assert_called_once_with(instance)
             self.assertEqual(5, mock_rmtree.call_count)
@@ -24104,7 +24108,7 @@ class LibvirtDriverTestCase(test.NoDBTestCase, TraitsComparisonMixin):
             mock_exists.return_value = True
             mock_get_path.return_value = '/fake/inst'
 
-            drvr._cleanup_resize(self.context, instance, fake_net)
+            drvr._cleanup_resize(self.context, None, instance, fake_net)
 
             mock_get_path.assert_called_once_with(instance)
             self.assertEqual(5, mock_rmtree.call_count)
@@ -24133,7 +24137,7 @@ class LibvirtDriverTestCase(test.NoDBTestCase, TraitsComparisonMixin):
             mock_get_path.return_value = '/fake/inst'
 
             drvr._cleanup_resize(
-                self.context, instance, _fake_network_info(self))
+                self.context, None, instance, _fake_network_info(self))
             mock_get_path.assert_called_once_with(instance)
             mock_remove.assert_called_once_with(
                 libvirt_utils.RESIZE_SNAPSHOT_NAME)
@@ -24164,7 +24168,7 @@ class LibvirtDriverTestCase(test.NoDBTestCase, TraitsComparisonMixin):
             mock_get_path.return_value = '/fake/inst'
 
             drvr._cleanup_resize(
-                self.context, instance, _fake_network_info(self))
+                self.context, None, instance, _fake_network_info(self))
             mock_get_path.assert_called_once_with(instance)
             self.assertFalse(mock_remove.called)
             self.assertEqual(5, mock_rmtree.call_count)
