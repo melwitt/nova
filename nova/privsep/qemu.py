cc3b323f88ecdca484cfd2dd494d7fc14a9d805b
melanie witt <melwittt@gmail.com>
2023-01-16 06:30:11 +0000

    Add encryption support to convert_image
    This change enables ephemeral encryption support to convert:
    
      * encrypted source image to unencrypted destination image
      * unencrypted source image to encrypted destination image
      * encrypted source image to encrypted destination image
    
    This also adds nova/privsep/qemu.py to the mypy file list and makes
    necessary changes for mypy checks to pass.
    
    Related to blueprint ephemeral-storage-encryption
    
    Change-Id: I9edc87006b1f7de69bc52f916f45c2cbb66abe23

--- nova/privsep/qemu.py
+++ nova/privsep/qemu.py
@@ -17,6 +17,8 @@ Helpers for qemu tasks.
 """
 
 import os
+import tempfile
+import typing as ty
 
 from oslo_concurrency import processutils
 from oslo_log import log as logging
@@ -33,16 +35,44 @@ QEMU_IMG_LIMITS = processutils.ProcessLimits(
     address_space=1 * units.Gi)
 
 
+class EncryptionOptions(ty.TypedDict):
+    secret: str
+    format: str
+
+
 @nova.privsep.sys_admin_pctxt.entrypoint
 def convert_image(source, dest, in_format, out_format, instances_path,
-                  compress):
+                  compress, encryption=None, dest_encryption=None):
     unprivileged_convert_image(source, dest, in_format, out_format,
-                               instances_path, compress)
+                               instances_path, compress, encryption=encryption,
+                               dest_encryption=dest_encryption)
 
 
 # NOTE(mikal): this method is deliberately not wrapped in a privsep entrypoint
-def unprivileged_convert_image(source, dest, in_format, out_format,
-                               instances_path, compress):
+def unprivileged_convert_image(
+    source: str,
+    dest: str,
+    in_format: ty.Optional[str],
+    out_format: str,
+    instances_path: str,
+    compress: bool,
+    encryption: ty.Optional[EncryptionOptions] = None,
+    dest_encryption: ty.Optional[EncryptionOptions] = None,
+) -> None:
+    """Disk image conversion with qemu-img
+
+    :param source: Location of the disk image to convert
+    :param dest: Desired location of the converted disk image
+    :param in_format: Disk image format of the source image
+    :param out_format: Desired disk image format of the converted disk image
+    :param instances_path: Location where instances are stored on disk
+    :param compress: Whether to compress the converted disk image
+    :param encryption: (Optional) Dict detailing various encryption attributes
+                       for the source image such as the format and passphrase.
+    :param dest_encryption: (Optional) Dict detailing various encryption
+                            attributes for the destination image such as the
+                            format and passphrase.
+    """
     # NOTE(mdbooth, kchamart): `qemu-img convert` defaults to
     # 'cache=writeback' for the source image, and 'cache=unsafe' for the
     # target, which means that data is not synced to disk at completion.
@@ -69,16 +99,103 @@ def unprivileged_convert_image(source, dest, in_format, out_format,
         cache_mode = 'none'
     else:
         cache_mode = 'writeback'
-    cmd = ('qemu-img', 'convert', '-t', cache_mode, '-O', out_format)
+    cmd = ['qemu-img', 'convert', '-t', cache_mode, '-O', out_format]
 
+    # qemu-img: --image-opts and --format are mutually exclusive
+    # If the source is encrypted, we will need to pass encryption related
+    # options using --image-opts.
+    driver_str = ''
     if in_format is not None:
-        cmd = cmd + ('-f', in_format)
+        if not encryption:
+            cmd += ['-f', in_format]
+        else:
+            driver_str = f'driver={in_format},'
 
     if compress:
-        cmd += ('-c',)
+        cmd += ['-c']
 
-    cmd = cmd + (source, dest)
-    processutils.execute(*cmd)
+    src_secret_file = None
+    dest_secret_file = None
+    encryption_opts: ty.List[str] = []
+    try:
+        if encryption:
+            src_secret_file = tempfile.NamedTemporaryFile(
+                mode='tr+', encoding='utf-8')
+
+            # Write out the passphrase secret to a temp file
+            src_secret_file.write(encryption['secret'])
+
+            # Ensure the secret is written to disk, we can't .close() here as
+            # that removes the file when using NamedTemporaryFile
+            src_secret_file.flush()
+
+            # When --image-opts is used, the source filename must be passed as
+            # part of the option string instead of as a positional arg.
+            encryption_opts = [
+                '--object', f"secret,id=sec,file={src_secret_file.name}",
+                '--image-opts',
+                f"{driver_str}file.driver=file,file.filename={source},"
+                "encrypt.key-secret=sec",
+            ]
+
+        if dest_encryption:
+            dest_secret_file = tempfile.NamedTemporaryFile(
+                mode='tr+', encoding='utf-8')
+
+            # Write out the passphrase secret to a temp file
+            dest_secret_file.write(dest_encryption['secret'])
+
+            # Ensure the secret is written to disk, we can't .close()
+            # here as that removes the file when using
+            # NamedTemporaryFile
+            dest_secret_file.flush()
+            encryption_opts += (
+                '--object', f"secret,id=sec_dest,file={dest_secret_file.name}",
+                '-o', 'encrypt.key-secret=sec_dest',
+                '-o', f"encrypt.format={dest_encryption.get('format')}",
+            )
+            # Supported luks options:
+            #  cipher-alg=<str>       - Name of cipher algorithm and
+            #                           key length
+            #  cipher-mode=<str>      - Name of encryption cipher mode
+            #  hash-alg=<str>         - Name of hash algorithm to use
+            #                           for PBKDF
+            #  iter-time=<num>        - Time to spend in PBKDF in
+            #                           milliseconds
+            #  ivgen-alg=<str>        - Name of IV generator algorithm
+            #  ivgen-hash-alg=<str>   - Name of IV generator hash
+            #                           algorithm
+            #
+            # NOTE(melwitt): Sensible defaults (that match the qemu
+            # defaults) are hardcoded at this time for simplicity and
+            # consistency when instances are migrated. Configuration of
+            # luks options could be added in a future release.
+            encryption_options = {
+                'cipher-alg': 'aes-256',
+                'cipher-mode': 'xts',
+                'hash-alg': 'sha256',
+                'iter-time': 2000,
+                'ivgen-alg': 'plain64',
+                'ivgen-hash-alg': 'sha256',
+            }
+            for option, value in encryption_options.items():
+                encryption_opts += [
+                    '-o', f'encrypt.{option}={value}',
+                ]
+
+        if encryption or dest_encryption:
+            cmd += encryption_opts
+
+        # If the source is not encrypted, it's passed as a positional argument.
+        if not encryption:
+            cmd += [source]
+
+        processutils.execute(*cmd + [dest])
+    finally:
+        if src_secret_file:
+            src_secret_file.close()
+        if dest_secret_file:
+            dest_secret_file.close()
 
 
 @nova.privsep.sys_admin_pctxt.entrypoint
@@ -100,12 +217,12 @@ def unprivileged_qemu_img_info(path, format=None):
             os.path.exists(os.path.join(path, "DiskDescriptor.xml"))):
             path = os.path.join(path, "root.hds")
 
-        cmd = (
+        cmd = [
             'env', 'LC_ALL=C', 'LANG=C', 'qemu-img', 'info', path,
             '--force-share', '--output=json',
-        )
+            ]
         if format is not None:
-            cmd = cmd + ('-f', format)
+            cmd = cmd + ['-f', format]
         out, err = processutils.execute(*cmd, prlimit=QEMU_IMG_LIMITS)
     except processutils.ProcessExecutionError as exp:
         if exp.exit_code == -9:
