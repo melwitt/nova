2b4539787a881a77a6726eac2f4a564992f2f578
melanie witt <melwittt@gmail.com>
2023-01-16 06:37:08 +0000

    Support (resize|cold migration) with ephemeral encryption for qcow2
    Related to blueprint ephemeral-encryption-libvirt
    
    Change-Id: I76075fb9d6cc070af58da4950c096952fe5b4036

--- nova/tests/unit/virt/libvirt/test_driver.py
+++ nova/tests/unit/virt/libvirt/test_driver.py
@@ -735,7 +735,7 @@ class LibvirtConnTestCase(test.NoDBTestCase,
         os_vif.initialize()
 
         self.stub_out('nova.virt.disk.api.extend',
-                      lambda image, size, use_cow=False: None)
+                      lambda image, size, use_cow=False, encryption=None: None)
 
         self.stub_out('nova.virt.libvirt.imagebackend.Image.'
                       'resolve_driver_format',
@@ -14311,6 +14311,7 @@ class LibvirtConnTestCase(test.NoDBTestCase,
              '/fake/instance/dir/foo',
              disk_info['type'],
              disk_info['virt_disk_size'],
+             encryption=None,
         )
         mock_exists.assert_called_once_with('/fake/instance/dir/foo')
 
@@ -14419,7 +14420,8 @@ class LibvirtConnTestCase(test.NoDBTestCase,
                 mock.call(self.context, ramdisk_path, instance.ramdisk_id,
                           trusted_certs)
             ])
-            resize_image_mock.assert_called_once_with(virt_disk_size)
+            resize_image_mock.assert_called_once_with(
+                virt_disk_size, encryption=None)
 
         mock_utime.assert_called()
         mock_create_cow_image.assert_called_once_with(
@@ -14557,7 +14559,8 @@ class LibvirtConnTestCase(test.NoDBTestCase,
 
             create_ephemeral_mock.assert_called_once_with(
                 ephemeral_size=1, fs_label='ephemeral_foo',
-                os_type='linux', target=ephemeral_backing)
+                os_type='linux', target=ephemeral_backing,
+                context=self.context)
 
             fetch_image_mock.assert_called_once_with(
                 context=self.context, image_id=instance.image_ref,
@@ -15129,6 +15132,7 @@ class LibvirtConnTestCase(test.NoDBTestCase,
 
         def check_instance_dir(context, instance,
                                instance_dir, disk_info,
+                               block_device_info=None,
                                fallback_from_host=False):
             self.assertTrue(instance_dir)
         # creating mocks
@@ -15149,6 +15153,7 @@ class LibvirtConnTestCase(test.NoDBTestCase,
                                           migrate_data=migrate_data)
             create_image_mock.assert_has_calls(
                 [mock.call(self.context, instance, mock.ANY, {},
+                           block_device_info=None,
                            fallback_from_host=instance.host)])
             self.assertIsInstance(res, objects.LibvirtLiveMigrateData)
 
@@ -23619,7 +23624,7 @@ class LibvirtDriverTestCase(test.NoDBTestCase, TraitsComparisonMixin):
         self.drvr.confirm_migration(self.context, "migration_ref", ins_ref,
                                     _fake_network_info(self))
         mock_cleanup.assert_called_once_with(
-            self.context, ins_ref, _fake_network_info(self))
+            self.context, "migration_ref", ins_ref, _fake_network_info(self))
 
     @mock.patch('time.sleep', new=mock.Mock())
     def test_cleanup_resize_same_host(self):
@@ -23641,7 +23646,7 @@ class LibvirtDriverTestCase(test.NoDBTestCase, TraitsComparisonMixin):
             mock_get_path.return_value = '/fake/inst'
 
             drvr._cleanup_resize(
-                self.context, instance, _fake_network_info(self))
+                self.context, None, instance, _fake_network_info(self))
             mock_get_path.assert_called_once_with(instance)
             self.assertEqual(5, mock_rmtree.call_count)
 
@@ -23671,7 +23676,7 @@ class LibvirtDriverTestCase(test.NoDBTestCase, TraitsComparisonMixin):
             mock_exists.return_value = True
             mock_get_path.return_value = '/fake/inst'
 
-            drvr._cleanup_resize(self.context, instance, fake_net)
+            drvr._cleanup_resize(self.context, None, instance, fake_net)
 
             mock_get_path.assert_called_once_with(instance)
             self.assertEqual(5, mock_rmtree.call_count)
@@ -23708,7 +23713,7 @@ class LibvirtDriverTestCase(test.NoDBTestCase, TraitsComparisonMixin):
             error = exception.InternalError("fake error")
             mock_unplug.side_effect = error
 
-            drvr._cleanup_resize(self.context, instance, fake_net)
+            drvr._cleanup_resize(self.context, None, instance, fake_net)
 
             mock_get_path.assert_called_once_with(instance)
             self.assertEqual(5, mock_rmtree.call_count)
@@ -23747,7 +23752,7 @@ class LibvirtDriverTestCase(test.NoDBTestCase, TraitsComparisonMixin):
             mock_exists.return_value = True
             mock_get_path.return_value = '/fake/inst'
 
-            drvr._cleanup_resize(self.context, instance, fake_net)
+            drvr._cleanup_resize(self.context, None, instance, fake_net)
 
             mock_get_path.assert_called_once_with(instance)
             self.assertEqual(5, mock_rmtree.call_count)
@@ -23776,7 +23781,7 @@ class LibvirtDriverTestCase(test.NoDBTestCase, TraitsComparisonMixin):
             mock_get_path.return_value = '/fake/inst'
 
             drvr._cleanup_resize(
-                self.context, instance, _fake_network_info(self))
+                self.context, None, instance, _fake_network_info(self))
             mock_get_path.assert_called_once_with(instance)
             mock_remove.assert_called_once_with(
                 libvirt_utils.RESIZE_SNAPSHOT_NAME)
@@ -23807,7 +23812,7 @@ class LibvirtDriverTestCase(test.NoDBTestCase, TraitsComparisonMixin):
             mock_get_path.return_value = '/fake/inst'
 
             drvr._cleanup_resize(
-                self.context, instance, _fake_network_info(self))
+                self.context, None, instance, _fake_network_info(self))
             mock_get_path.assert_called_once_with(instance)
             self.assertFalse(mock_remove.called)
             self.assertEqual(5, mock_rmtree.call_count)
