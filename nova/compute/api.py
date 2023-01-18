3b7c8cac575dbacf1ec370c9c28978c969b0831b
melanie witt <melwittt@gmail.com>
2023-01-18 02:54:31 +0000

    Support create with ephemeral encryption for qcow2
    This adds support for the qcow2 imagebackend to create encrypted
    ephemeral disks for:
    
      * root disk created from an unencrypted source image
        (encrypted source image support is future work)
      * ephemeral disk
      * swap disk
    
    A new unit test test_driver_capabilities_flat is also added ahead of
    support for the RAW image backend. The Flat image backend was the only
    backend that did not have a driver capabilities unit test.
    
    Related to blueprint ephemeral-encryption-libvirt
    
    Change-Id: Ie5788c791fae000573c488ea31af7a15799e1404

--- nova/compute/api.py
+++ nova/compute/api.py
@@ -312,6 +312,26 @@ def reject_vdpa_instances(operation, until=None):
     return outer
 
 
+def reject_ephemeral_encryption_instances(operation):
+    """Reject requests to decorated funcs if instance uses ephemeral encryption
+
+    Raise OperationNotSupportedForEphemeralEncryption if instance uses
+    ephemeral encryption.
+    """
+
+    def outer(f):
+        @functools.wraps(f)
+        def inner(self, context, instance, *args, **kw):
+            if hardware.get_ephemeral_encryption_constraint(
+                instance.flavor, instance.image_meta,
+            ):
+                raise exception.OperationNotSupportedForEphemeralEncryption(
+                    instance_uuid=instance.uuid, operation=operation)
+            return f(self, context, instance, *args, **kw)
+        return inner
+    return outer
+
+
 def load_cells():
     global CELLS
     if not CELLS:
@@ -3308,6 +3328,7 @@ class API:
                     raise exception.InstanceNotFound(instance_id=instance.uuid)
         return instance
 
+    @reject_ephemeral_encryption_instances(instance_actions.BACKUP)
     # NOTE(melwitt): We don't check instance lock for backup because lock is
     #                intended to prevent accidental change/delete of instances
     @check_instance_state(vm_state=[vm_states.ACTIVE, vm_states.STOPPED,
@@ -3349,6 +3370,7 @@ class API:
                                             rotation)
         return image_meta
 
+    @reject_ephemeral_encryption_instances(instance_actions.CREATE_IMAGE)
     # NOTE(melwitt): We don't check instance lock for snapshot because lock is
     #                intended to prevent accidental change/delete of instances
     @check_instance_state(vm_state=[vm_states.ACTIVE, vm_states.STOPPED,
@@ -3406,6 +3428,7 @@ class API:
 
         return image_meta
 
+    @reject_ephemeral_encryption_instances(instance_actions.CREATE_IMAGE)
     # NOTE(melwitt): We don't check instance lock for snapshot because lock is
     #                intended to prevent accidental change/delete of instances
     @check_instance_state(vm_state=[vm_states.ACTIVE, vm_states.STOPPED,
@@ -3588,6 +3611,7 @@ class API:
             if img_arch:
                 fields_obj.Architecture.canonicalize(img_arch)
 
+    @reject_ephemeral_encryption_instances(instance_actions.REBUILD)
     @reject_vtpm_instances(instance_actions.REBUILD)
     @block_accelerators(until_service=SUPPORT_ACCELERATOR_SERVICE_FOR_REBUILD)
     # TODO(stephenfin): We should expand kwargs out to named args
@@ -4158,6 +4182,7 @@ class API:
 
         return node
 
+    @reject_ephemeral_encryption_instances(instance_actions.RESIZE)
     # TODO(stephenfin): This logic would be so much easier to grok if we
     # finally split resize and cold migration into separate code paths
     @block_extended_resource_request
@@ -4400,6 +4425,7 @@ class API:
             allow_same_host = CONF.allow_resize_to_same_host
         return allow_same_host
 
+    @reject_ephemeral_encryption_instances(instance_actions.SHELVE)
     @block_port_accelerators()
     @reject_vtpm_instances(instance_actions.SHELVE)
     @block_accelerators(until_service=54)
@@ -4740,6 +4766,7 @@ class API:
         self._record_action_start(context, instance, instance_actions.RESUME)
         self.compute_rpcapi.resume_instance(context, instance)
 
+    @reject_ephemeral_encryption_instances(instance_actions.RESCUE)
     @reject_vtpm_instances(instance_actions.RESCUE)
     @check_instance_lock
     @check_instance_state(vm_state=[vm_states.ACTIVE, vm_states.STOPPED,
@@ -5523,6 +5550,7 @@ class API:
 
         return _metadata
 
+    @reject_ephemeral_encryption_instances(instance_actions.LIVE_MIGRATION)
     @block_extended_resource_request
     @block_port_accelerators()
     @reject_vdpa_instances(
@@ -5660,6 +5688,7 @@ class API:
         self.compute_rpcapi.live_migration_abort(context,
                 instance, migration.id)
 
+    @reject_ephemeral_encryption_instances(instance_actions.EVACUATE)
     @block_extended_resource_request
     @block_port_accelerators()
     @reject_vtpm_instances(instance_actions.EVACUATE)
@@ -5849,6 +5878,7 @@ class API:
         bdm = self._get_bdm_by_volume_id(
             context, volume_id, expected_attrs=['instance'])
 
+        @reject_ephemeral_encryption_instances(instance_actions.CREATE_IMAGE)
         # We allow creating the snapshot in any vm_state as long as there is
         # no task being performed on the instance and it has a host.
         @check_instance_host()
