71cc8e505e61173d06b81f4a2864b18b7ef9097a
melanie witt <melwittt@gmail.com>
2023-02-13 02:21:09 +0000

    Support rescue with ephemeral encryption
    This also makes rescue use the image metadata from the
    CONF.libvirt.rescue_image_id if it ends up being chosen as the rescue
    image. Previously, its image metadata was never used.
    
    Related to blueprint ephemeral-encryption-libvirt
    
    Change-Id: I254eb563f90772d3a2273c4286142f6a761285df

--- nova/conf/libvirt.py
+++ nova/conf/libvirt.py
@@ -48,6 +48,12 @@ If the rescue REST API operation doesn't provide an ID of an image to
 use, the image which is referenced by this ID is used. If this
 option is not set, the image from the instance is used.
 
+.. warning::
+    Be careful when using an encrypted image as the default rescue image. It
+    would require that all end users have permissions to access the secret
+    indicated in the hw_ephemeral_encryption_secret_uuid image property of the
+    rescue image.
+
 Possible values:
 
 * An ID of an image or nothing. If it points to an *Amazon Machine
