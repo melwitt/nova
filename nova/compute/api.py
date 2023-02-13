384010b6464fa77125756a0182b6812b0cd6619c
melanie witt <melwittt@gmail.com>
2023-02-13 02:21:09 +0000

    Support rescue with ephemeral encryption for qcow2
    This also makes rescue use the image metadata from the
    CONF.libvirt.rescue_image_id if it ends up being chosen as the rescue
    image. Previously, its image metadata was never used.
    
    Related to blueprint ephemeral-encryption-libvirt
    
    Change-Id: I254eb563f90772d3a2273c4286142f6a761285df

--- nova/compute/api.py
+++ nova/compute/api.py
@@ -1662,42 +1662,6 @@ class API:
 
         return objects.InstanceGroup.get_by_uuid(context, group_hint)
 
-    def _update_ephemeral_encryption_bdms(
-        self,
-        flavor: 'objects.Flavor',
-        image_meta_dict: ty.Dict[str, ty.Any],
-        block_device_mapping: 'objects.BlockDeviceMappingList',
-    ) -> None:
-        """Update local BlockDeviceMappings when ephemeral encryption requested
-
-        Enable ephemeral encryption in all local BlockDeviceMappings
-        when requested in the flavor or image. Also optionally set the format
-        and options if also provided.
-
-        :param flavor: The instance flavor for the request
-        :param image_meta_dict: The image metadata for the request
-        :block_device_mapping: The current block_device_mapping for the request
-        """
-        image_meta = _get_image_meta_obj(image_meta_dict)
-        if not hardware.get_ephemeral_encryption_constraint(
-                flavor, image_meta):
-            return
-
-        # NOTE(lyarwood): Attempt to find the format in the flavor and image,
-        # if one isn't found then the compute will need to provide and save a
-        # default format during a the initial build.
-        eph_format = hardware.get_ephemeral_encryption_format(
-            flavor, image_meta)
-
-        # NOTE(lyarwood): The term ephemeral is overloaded in the codebase,
-        # what it actually means in the context of ephemeral encryption is
-        # anything local to the compute host so use the is_local property.
-        # TODO(lyarwood): Add .get_local_devices() to BlockDeviceMappingList
-        for bdm in [b for b in block_device_mapping if b.is_local]:
-            bdm.encrypted = True
-            if eph_format:
-                bdm.encryption_format = eph_format
-
     def _create_instance(self, context, flavor,
                image_href, kernel_id, ramdisk_id,
                min_count, max_count,
@@ -1785,8 +1749,9 @@ class API:
 
         # Update any local BlockDeviceMapping objects if ephemeral encryption
         # has been requested though flavor extra specs or image properties
-        self._update_ephemeral_encryption_bdms(
-            flavor, boot_meta, block_device_mapping)
+        image_meta = _get_image_meta_obj(boot_meta)
+        compute_utils.update_ephemeral_encryption_bdms(
+            flavor, image_meta, block_device_mapping)
 
         # We can't do this check earlier because we need bdms from all sources
         # to have been merged in order to get the root bdm.
@@ -3794,7 +3759,9 @@ class API:
                 context, instance, flavor, image, bdms)
             # Update any local BlockDeviceMapping objects if ephemeral
             # encryption has been requested though image properties
-            self._update_ephemeral_encryption_bdms(flavor, image, bdms)
+            image_meta = _get_image_meta_obj(image)
+            compute_utils.update_ephemeral_encryption_bdms(
+                flavor, image_meta, bdms)
 
         kernel_id, ramdisk_id = self._handle_kernel_and_ramdisk(
                 context, None, None, image)
@@ -4889,7 +4856,6 @@ class API:
         self._record_action_start(context, instance, instance_actions.RESUME)
         self.compute_rpcapi.resume_instance(context, instance)
 
-    @reject_ephemeral_encryption_instances(instance_actions.RESCUE)
     @reject_vtpm_instances(instance_actions.RESCUE)
     @check_instance_lock
     @check_instance_state(vm_state=[vm_states.ACTIVE, vm_states.STOPPED,
