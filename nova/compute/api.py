fc006476f3a5a818415f030920afcd5a7e95c5f1
melanie witt <melwittt@gmail.com>
2024-01-12 22:38:07 +0000

    Support live migration with ephemeral encryption for qcow2
    Related to blueprint ephemeral-encryption-libvirt
    
    Change-Id: I6c0bc57951a7a3c3778b13b053da2a84d481daba

--- nova/compute/api.py
+++ nova/compute/api.py
@@ -5600,7 +5600,6 @@ class API:
 
         return _metadata
 
-    @reject_ephemeral_encryption_instances(instance_actions.LIVE_MIGRATION)
     @block_extended_resource_request
     @block_port_accelerators()
     @reject_vdpa_instances(
