45b6e2009fd5a0d7cb1572ddd9c8195ee4235b4a
melanie witt <melwittt@gmail.com>
2024-01-12 22:38:07 +0000

    WIP Support live migration with ephemeral encryption
    Related to blueprint ephemeral-encryption-libvirt
    
    Change-Id: I6c0bc57951a7a3c3778b13b053da2a84d481daba

--- nova/tests/unit/virt/libvirt/test_driver.py
+++ nova/tests/unit/virt/libvirt/test_driver.py
@@ -15197,6 +15197,7 @@ class LibvirtConnTestCase(test.NoDBTestCase,
 
         def check_instance_dir(context, instance,
                                instance_dir, disk_info,
+                               block_device_info=None,
                                fallback_from_host=False):
             self.assertTrue(instance_dir)
         # creating mocks
@@ -15217,6 +15218,7 @@ class LibvirtConnTestCase(test.NoDBTestCase,
                                           migrate_data=migrate_data)
             create_image_mock.assert_has_calls(
                 [mock.call(self.context, instance, mock.ANY, {},
+                           block_device_info=None,
                            fallback_from_host=instance.host)])
             self.assertIsInstance(res, objects.LibvirtLiveMigrateData)
 
@@ -15357,7 +15359,7 @@ class LibvirtConnTestCase(test.NoDBTestCase,
 
         drvr = libvirt_driver.LibvirtDriver(fake.FakeVirtAPI(), False)
 
-        inst_ref = {'id': 'foo'}
+        inst_ref = objects.Instance(**self.test_instance)
         cntx = context.get_admin_context()
 
         # Set up the mock expectations
@@ -15382,20 +15384,19 @@ class LibvirtConnTestCase(test.NoDBTestCase,
         vol_2_conn_info = {'data': {'volume_id': uuids.vol_2_id}}
         mock_get_bdm.return_value = [{'connection_info': vol_1_conn_info},
                                      {'connection_info': vol_2_conn_info}]
+        instance = objects.Instance(**self.test_instance)
 
         # Raise an exception with the first call to disconnect_volume
         mock_disconnect_volume.side_effect = [test.TestingException, None]
 
         drvr = libvirt_driver.LibvirtDriver(fake.FakeVirtAPI(), False)
-        drvr.post_live_migration(mock.sentinel.ctxt, mock.sentinel.instance,
+        drvr.post_live_migration(mock.sentinel.ctxt, instance,
                                  mock.sentinel.bdi)
 
         # Assert disconnect_volume is called twice despite the exception
         mock_disconnect_volume.assert_has_calls([
-            mock.call(mock.sentinel.ctxt, vol_1_conn_info,
-                      mock.sentinel.instance),
-            mock.call(mock.sentinel.ctxt, vol_2_conn_info,
-                      mock.sentinel.instance)])
+            mock.call(mock.sentinel.ctxt, vol_1_conn_info, instance),
+            mock.call(mock.sentinel.ctxt, vol_2_conn_info, instance)])
 
         # Assert that we log the failure to disconnect the first volume
         self.assertIn("Ignoring exception while attempting to disconnect "
