a94790ae4f1192595bb7ab21e41451b29f3f49a6
melanie witt <melwittt@gmail.com>
2024-01-13 00:47:49 +0000

    WIP for migration patch
    Change-Id: I859ade7a52273acc375ef96ab8784dc98c009077

--- nova/virt/libvirt/driver.py
+++ nova/virt/libvirt/driver.py
@@ -4145,7 +4145,7 @@ class LibvirtDriver(driver.ComputeDriver):
         # scratch, and hopefully fix, most aspects of a non-functioning guest.
         self.destroy(context, instance, network_info, destroy_disks=False,
                      block_device_info=block_device_info,
-                     destroy_secrets=False)
+                     destroy_secrets=False, destroy_ephemeral_secrets=False)
 
         # Convert the system metadata to image metadata
         # NOTE(mdbooth): This is a workaround for stateless Nova compute
