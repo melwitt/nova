9a8524564f1292a049c996df8f5c9439ada4a86e
melanie witt <melwittt@gmail.com>
2024-01-29 08:13:20 +0000

    Support encrypted backing files for qcow2
    This is the first part for adding support of encrypted backing files.
    It is focused on tracking backing file secrets in BDMs and making
    image rebase and image convert work with encrypted backing files.
    
    A following patch will add support for encrypted source images (which
    will be used to create encrypted backing files).
    
    Related to blueprint ephemeral-encryption-libvirt
    
    Change-Id: Ief348c2555436ad4764438de1931a0489e1276b7

--- nova/compute/api.py
+++ nova/compute/api.py
@@ -3390,7 +3390,6 @@ class API:
                     raise exception.InstanceNotFound(instance_id=instance.uuid)
         return instance
 
-    @reject_ephemeral_encryption_instances(instance_actions.BACKUP)
     # NOTE(melwitt): We don't check instance lock for backup because lock is
     #                intended to prevent accidental change/delete of instances
     @check_instance_state(vm_state=[vm_states.ACTIVE, vm_states.STOPPED,
@@ -3432,7 +3431,6 @@ class API:
                                             rotation)
         return image_meta
 
-    @reject_ephemeral_encryption_instances(instance_actions.CREATE_IMAGE)
     # NOTE(melwitt): We don't check instance lock for snapshot because lock is
     #                intended to prevent accidental change/delete of instances
     @check_instance_state(vm_state=[vm_states.ACTIVE, vm_states.STOPPED,
@@ -4543,7 +4541,6 @@ class API:
             allow_same_host = CONF.allow_resize_to_same_host
         return allow_same_host
 
-    @reject_ephemeral_encryption_instances(instance_actions.SHELVE)
     @block_port_accelerators()
     @reject_vtpm_instances(instance_actions.SHELVE)
     @block_accelerators(until_service=54)
