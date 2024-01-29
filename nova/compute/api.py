d9bbd3ab93cddbf90298896c3b10ae7210d8a32b
melanie witt <melwittt@gmail.com>
2024-01-29 08:13:20 +0000

    Support encrypted backing files for qcow2
    This is the first part for adding support of encrypted backing files.
    It is focused on tracking backing file secrets in BDMs and making
    image rebase and image convert work with encrypted backing files.
    
    A following patch will add support for encrypted source images (which
    will be used to create encrypted backing files).
    
    Related to blueprint ephemeral-encryption-libvirt
    
    Change-Id: Ief348c2555436ad4764438de1931a0489e1276b7

--- nova/compute/api.py
+++ nova/compute/api.py
@@ -312,26 +312,6 @@ def reject_vdpa_instances(operation, until=None):
     return outer
 
 
-def reject_ephemeral_encryption_instances(operation):
-    """Reject requests to decorated funcs if instance uses ephemeral encryption
-
-    Raise OperationNotSupportedForEphemeralEncryption if instance uses
-    ephemeral encryption.
-    """
-
-    def outer(f):
-        @functools.wraps(f)
-        def inner(self, context, instance, *args, **kw):
-            if hardware.get_ephemeral_encryption_constraint(
-                instance.flavor, instance.image_meta,
-            ):
-                raise exception.OperationNotSupportedForEphemeralEncryption(
-                    instance_uuid=instance.uuid, operation=operation)
-            return f(self, context, instance, *args, **kw)
-        return inner
-    return outer
-
-
 def check_ephemeral_encryption_key_access(ctxt, flavor, image_meta):
     if hardware.get_ephemeral_encryption_constraint(flavor, image_meta):
         # NOTE(melwitt): Infer the key manager service configuration from ours
@@ -3363,7 +3343,6 @@ class API:
                     raise exception.InstanceNotFound(instance_id=instance.uuid)
         return instance
 
-    @reject_ephemeral_encryption_instances(instance_actions.BACKUP)
     # NOTE(melwitt): We don't check instance lock for backup because lock is
     #                intended to prevent accidental change/delete of instances
     @check_instance_state(vm_state=[vm_states.ACTIVE, vm_states.STOPPED,
@@ -3405,7 +3384,6 @@ class API:
                                             rotation)
         return image_meta
 
-    @reject_ephemeral_encryption_instances(instance_actions.CREATE_IMAGE)
     # NOTE(melwitt): We don't check instance lock for snapshot because lock is
     #                intended to prevent accidental change/delete of instances
     @check_instance_state(vm_state=[vm_states.ACTIVE, vm_states.STOPPED,
@@ -3463,7 +3441,6 @@ class API:
 
         return image_meta
 
-    @reject_ephemeral_encryption_instances(instance_actions.CREATE_IMAGE)
     # NOTE(melwitt): We don't check instance lock for snapshot because lock is
     #                intended to prevent accidental change/delete of instances
     @check_instance_state(vm_state=[vm_states.ACTIVE, vm_states.STOPPED,
@@ -4527,7 +4504,6 @@ class API:
             allow_same_host = CONF.allow_resize_to_same_host
         return allow_same_host
 
-    @reject_ephemeral_encryption_instances(instance_actions.SHELVE)
     @block_port_accelerators()
     @reject_vtpm_instances(instance_actions.SHELVE)
     @block_accelerators(until_service=54)
@@ -5969,7 +5945,6 @@ class API:
         bdm = self._get_bdm_by_volume_id(
             context, volume_id, expected_attrs=['instance'])
 
-        @reject_ephemeral_encryption_instances(instance_actions.CREATE_IMAGE)
         # We allow creating the snapshot in any vm_state as long as there is
         # no task being performed on the instance and it has a host.
         @check_instance_host()
--- nova/tests/unit/compute/test_api.py
+++ nova/tests/unit/compute/test_api.py
@@ -3174,10 +3174,6 @@ class _ComputeAPIUnitTestMixIn(object):
         }
         image_type = is_snapshot and 'snapshot' or 'backup'
         sent_meta = {
-            # The @reject_ephemeral_encryption_instances decorator makes
-            # setting 'id' here necessary because instance.image_meta will set
-            # the 'id' attribute via ImageMeta.from_instance().
-            'id': instance.image_ref,
             'visibility': 'private',
             'name': 'fake-name',
             'disk_format': 'fake',
@@ -3266,7 +3262,7 @@ class _ComputeAPIUnitTestMixIn(object):
 
         mock_create.assert_called_once_with(self.context, sent_meta)
         call = mock.call(instance.system_metadata)
-        self.assertEqual([call, call], mock_get_image.mock_calls)
+        self.assertEqual([call], mock_get_image.mock_calls)
 
         if not is_snapshot:
             mock_is_volume.assert_called_once_with(self.context, instance)
