e3aa0e435acce7e4732410f8ff9d996801707082
melanie witt <melwittt@gmail.com>
2024-01-30 23:42:43 +0000

    Add backing_encryption_secret_uuid to BlockDeviceMapping
    This will be needed to support encrypted backing files for ephemeral
    encryption.
    
    Related to blueprint ephemeral-storage-encryption
    
    Change-Id: I3f387f560c4e1cae6ae9c89eec69e5bdb354d897

--- nova/virt/libvirt/blockinfo.py
+++ nova/virt/libvirt/blockinfo.py
@@ -404,6 +404,8 @@ def get_info_from_bdm(instance, virt_type, image_meta, bdm,
     if bdm.get('encrypted'):
         bdm_info['encrypted'] = bdm.get('encrypted')
         bdm_info['encryption_secret_uuid'] = bdm.get('encryption_secret_uuid')
+        bdm_info['backing_encryption_secret_uuid'] = bdm.get(
+            'backing_encryption_secret_uuid')
         bdm_info['encryption_format'] = bdm.get('encryption_format')
         bdm_info['encryption_options'] = bdm.get('encryption_options')
 
