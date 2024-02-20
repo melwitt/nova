e354393dc6bcfcd023955d0da27007b65ceb3660
melanie witt <melwittt@gmail.com>
2024-02-20 10:31:52 +0000

    Support cross cell resize with ephemeral encryption for qcow2
    Related to blueprint ephemeral-encryption-libvirt
    
    Change-Id: I9c7610c1b0ffeca39ffcfc3389cf8321e7169646

--- nova/compute/manager.py
+++ nova/compute/manager.py
@@ -71,6 +71,7 @@ from nova.compute import vm_states
 from nova import conductor
 import nova.conf
 import nova.context
+from nova import crypto
 from nova import exception
 from nova import exception_wrapper
 from nova.i18n import _
@@ -6507,6 +6508,18 @@ class ComputeManager(manager.Manager):
             instance.image_ref = origin_image_ref
             compute_utils.delete_image(
                 ctxt, instance, self.image_api, snapshot_id)
+            # Clean up the ephemeral encryption secret for the image if there
+            # is one.
+            secret_uuid = image_meta.properties.get(
+                'hw_ephemeral_encryption_secret_uuid')
+            if secret_uuid:
+                try:
+                    crypto.delete_encryption_secret(
+                        ctxt, instance.uuid, secret_uuid)
+                except Exception:
+                    LOG.exception(
+                        f'Failed to delete encryption secret {secret_uuid} '
+                        'from the key manager.', instance=instance)
 
         migration.status = 'finished'
         migration.save()
