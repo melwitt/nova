fe372d4e04ab9d325e7729032c523eccce024104
melanie witt <melwittt@gmail.com>
2024-02-20 10:31:52 +0000

    Support cross cell resize with ephemeral encryption for qcow2
    Related to blueprint ephemeral-encryption-libvirt
    
    Change-Id: I9c7610c1b0ffeca39ffcfc3389cf8321e7169646

--- nova/tests/unit/virt/libvirt/test_driver.py
+++ nova/tests/unit/virt/libvirt/test_driver.py
@@ -17162,6 +17162,8 @@ class LibvirtConnTestCase(test.NoDBTestCase,
             self.assertFalse(service_mock.disabled)
             self.assertIsNone(service_mock.disabled_reason)
 
+    @mock.patch('nova.objects.BlockDeviceMappingList.get_by_instance_uuid',
+                new=mock.Mock(return_value=[]))
     @mock.patch.object(libvirt_driver.AsyncDeviceEventsHandler,
                        'cleanup_waiters')
     @mock.patch.object(libvirt_driver.LibvirtDriver, 'delete_instance_files')
@@ -20816,6 +20818,8 @@ class LibvirtConnTestCase(test.NoDBTestCase,
                               drvr.cleanup, 'ctxt', fake_inst, 'netinfo')
             unplug.assert_called_once_with(fake_inst, 'netinfo', True)
 
+    @mock.patch('nova.objects.BlockDeviceMappingList.get_by_instance_uuid',
+                new=mock.Mock(return_value=[]))
     @mock.patch('nova.virt.libvirt.driver.LibvirtDriver._undefine_domain')
     @mock.patch('nova.crypto.delete_vtpm_secret')
     @mock.patch('nova.virt.libvirt.driver.LibvirtDriver.delete_instance_files')
@@ -20842,6 +20846,8 @@ class LibvirtConnTestCase(test.NoDBTestCase,
         mock_delete_vtpm.assert_called_once_with('ctxt', fake_inst)
         mock_undefine.assert_called_once_with(fake_inst)
 
+    @mock.patch('nova.objects.BlockDeviceMappingList.get_by_instance_uuid',
+                new=mock.Mock(return_value=[]))
     @mock.patch('nova.virt.libvirt.driver.LibvirtDriver._undefine_domain')
     @mock.patch('nova.crypto.delete_vtpm_secret')
     @mock.patch('nova.virt.libvirt.driver.LibvirtDriver.delete_instance_files')
@@ -31095,9 +31101,12 @@ class EphemeralEncryptionTestCase(test.NoDBTestCase):
         self.drvr._host.delete_secret.assert_not_called()
 
     @mock.patch('nova.objects.instance.Instance.save', new=mock.Mock())
+    @mock.patch('nova.virt.driver.get_block_device_info')
+    @mock.patch('nova.objects.BlockDeviceMappingList.get_by_instance_uuid')
     def _test_cleanup_with_ephemeral_encryption(
-        self, has_key_mgr_secret=True, has_libvirt_secret=True,
-        destroy_disks=True, has_backing_secret=False
+        self, mock_get_bdms, mock_get_block_device_info,
+        has_key_mgr_secret=True, has_libvirt_secret=True, destroy_disks=True,
+        has_backing_secret=False
     ):
         mock_domain = mock.Mock(fakelibvirt.virDomain)
         mock_domain.ID.return_value = 123
@@ -31147,6 +31156,11 @@ class EphemeralEncryptionTestCase(test.NoDBTestCase):
         else:
             self.drvr._host.delete_secret.assert_not_called()
 
+        # Assert that we did not query for BDMs or block_device_info when
+        # block_device_info was provided.
+        mock_get_bdms.assert_not_called()
+        mock_get_block_device_info.assert_not_called()
+
     def test_cleanup_with_ephemeral_encryption(self):
         self._test_cleanup_with_ephemeral_encryption()
 
@@ -31161,6 +31175,16 @@ class EphemeralEncryptionTestCase(test.NoDBTestCase):
 
     def test_cleanup_with_ephemeral_encryption_no_destroy_disks(self):
         self._test_cleanup_with_ephemeral_encryption(destroy_disks=False)
+
+    @mock.patch('nova.objects.instance.Instance.save', new=mock.Mock())
+    @mock.patch('nova.virt.driver.get_block_device_info')
+    @mock.patch('nova.objects.BlockDeviceMappingList.get_by_instance_uuid')
+    def test_cleanup_with_ephemeral_encryption_no_block_device_info(
+            self, mock_get_bdms, mock_get_block_device_info):
+        self.drvr.cleanup(self.context, self.instance, [])
+        mock_get_bdms.assert_called_once_with(self.context, self.instance.uuid)
+        mock_get_block_device_info.assert_called_once_with(
+            self.instance, mock_get_bdms.return_value)
 
     def test__cleanup_delete_secret_fails(self):
         # Test exception handling when libvirt secret deletion fails during
