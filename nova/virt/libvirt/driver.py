1cedc72d6add9531edb0cddb02bceb4982dbd396
melanie witt <melwittt@gmail.com>
2024-02-23 01:01:10 +0000

    Report ephemeral disk encryption in the metadata API
    Related to blueprint ephemeral-storage-encryption
    
    Change-Id: I00bd36491c9b24f68bb3396965f47b8fb286e7ca

--- nova/virt/libvirt/driver.py
+++ nova/virt/libvirt/driver.py
@@ -13645,6 +13645,7 @@ class LibvirtDriver(driver.ComputeDriver):
         device.serial = bdm.volume_id
         if bus:
             device.bus = bus
+        device.encrypted = bdm.encrypted
         return device
 
     def _build_hostdev_metadata(self, dev, vifs_to_expose, vlans_by_mac):
