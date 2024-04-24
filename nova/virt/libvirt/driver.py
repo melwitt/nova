6e7d37677687977d270aec6076a5c0ede9efce7b
melanie witt <melwittt@gmail.com>
2024-04-24 20:56:44 +0000

    WIP for support encrypted source images patch
    - mypy fixes

--- nova/virt/libvirt/driver.py
+++ nova/virt/libvirt/driver.py
@@ -12138,7 +12138,7 @@ class LibvirtDriver(driver.ComputeDriver):
             context: nova_context.RequestContext,
             image_id: str,
             instance: 'objects.Instance',
-    ) -> ty.Optional[ty.Dict[str, str]]:
+    ) -> ty.Tuple['objects.ImageMeta', ty.Optional[ty.Dict[str, str]]]:
         # If the image properties contained an ephemeral encryption secret UUID
         # for the encrypted image, we retrieve it from the image. We don't use
         # the image metadata from the instance system metadata because that
@@ -12161,7 +12161,9 @@ class LibvirtDriver(driver.ComputeDriver):
                     f'Failed to find encryption secret {secret_uuid} in the '
                     f'key manager for image {image_id}')
                 raise exception.EphemeralEncryptionSecretNotFound(msg)
-            if not image_meta.properties.get('hw_ephemeral_encryption_format'):
+            encryption_format = image_meta.properties.get(
+                'hw_ephemeral_encryption_format')
+            if not encryption_format:
                 msg = _(
                     'If hw_ephemeral_encryption_secret_uuid is set in '
                     'image properties, hw_ephemeral_encryption_format must '
