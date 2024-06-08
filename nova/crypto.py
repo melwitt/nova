45bb607f112510a6122d192ce12d583a96aa2176
melanie witt <melwittt@gmail.com>
2024-06-08 00:18:26 +0000

    Follow up for encryption secret create consolidation
    This addresses review comments from change
    I35bd5c90f163d240d5d3666f78116855239c3184 to add missing unit test
    coverage and remove an unnecessary function argument.
    
    Related to blueprint ephemeral-storage-encryption
    
    Change-Id: I51cb0b87e46baa63ca388acc54ad72080a99f4cf

--- nova/crypto.py
+++ nova/crypto.py
@@ -172,7 +172,6 @@ def create_encryption_secret(
     context: nova_context.RequestContext,
     secret: ty.Union[str, bytes],
     name: str,
-    key_mgr: ty.Optional['key_manager.API'] = None,
 ) -> str:
     """Create a secret in the key manager service.
 
@@ -181,8 +180,7 @@ def create_encryption_secret(
     :param name: A name/description for the secret
     :returns: The UUID of the secret created in the key manager
     """
-    if key_mgr is None:
-        key_mgr = _get_key_manager()
+    key_mgr = _get_key_manager()
     # Castellan ManagedObject
     cmo = passphrase.Passphrase(secret, name=name)
     try:
@@ -236,8 +234,7 @@ def ensure_vtpm_secret(
     # and register it with the key manager.
     secret = base64.b64encode(os.urandom(_VTPM_SECRET_BYTE_LENGTH))
     secret_uuid = create_encryption_secret(
-        context, secret, "vTPM secret for instance %s" % instance.uuid,
-        key_mgr)
+        context, secret, "vTPM secret for instance %s" % instance.uuid)
     LOG.debug("Created vTPM secret with UUID %s",
               secret_uuid, instance=instance)
 
