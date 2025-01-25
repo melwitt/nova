57e3005dc8514632cd00c0e532698d5edfe28a2d
Artom Lifshitz <alifshit@redhat.com>
2025-01-25 09:30:14 -0500

    Add hw_tpm_secret_security image property
    It can hold the `user`, `host`, or `deployment` value, as explained in
    the spec.
    
    NOTE(artom) The spec proposed calling the new property
    hw_vtpm_secret_security. This patch changes it to
    hw_tpm_secret_security (without the v) to align with the other TPM
    image properties.
    
    Related to blueprint vtpm-live-migration
    
    Change-Id: I01721b0f244109023d97808c394281c4357c7958
    Signed-off-by: melanie witt <melwittt@gmail.com>

--- nova/objects/image_meta.py
+++ nova/objects/image_meta.py
@@ -200,15 +200,18 @@ class ImageMetaProps(base.NovaObject):
     # Version 1.39: Added igb value to 'hw_vif_model' enum
     # Version 1.40: Added 'hw_sound_model' field
     # Version 1.41: Added 'hw_usb_model' and 'hw_redirected_usb_ports' fields
+    # Version 1.42: Added 'hw_tpm_secret_security' field
 
     # NOTE(efried): When bumping this version, the version of
     # ImageMetaPropsPayload must also be bumped. See its docstring for details.
-    VERSION = '1.41'
+    VERSION = '1.42'
 
     def obj_make_compatible(self, primitive, target_version):
         super(ImageMetaProps, self).obj_make_compatible(primitive,
                                                         target_version)
         target_version = versionutils.convert_version_to_tuple(target_version)
+        if target_version < (1, 42):
+            primitive.pop('hw_tpm_secret_security', None)
         if target_version < (1, 41):
             primitive.pop('hw_usb_model', None)
             primitive.pop('hw_redirected_usb_ports', None)
@@ -484,6 +487,8 @@ class ImageMetaProps(base.NovaObject):
         'hw_tpm_model': fields.TPMModelField(),
         # version of emulated TPM to use.
         'hw_tpm_version': fields.TPMVersionField(),
+        # TPM secret security policy to use.
+        'hw_tpm_secret_security': fields.TPMSecretSecurityField(),
 
         # boolean - if true will enable ephemeral encryption for instance
         'hw_ephemeral_encryption': fields.FlexibleBooleanField(),
