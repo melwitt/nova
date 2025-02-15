d8f70e29abbac1f3b597c7bd3791bf09ef55a35c
Artom Lifshitz <alifshit@redhat.com>
2025-02-15 07:17:39 -0500

    TPM: support instances with `host` secret security
    Start supporting booting instances with the `host` TPM secret
    security. This means setting the `ephemeral` and `private` attributes
    on the Libvirt secret correctly, and not undefining the secret once
    the instance has spawned. The Libvirt fixture's Secret support is
    extended to be able to test all that in a functional test.
    
    For functional testing, we need to:
    
    * Extend our libvirt fixture's Secret object to properly set the usage
      id (which is just the instance UUID) when parsing vTPM secret XML.
    
    Related to blueprint vtpm-live-migration
    
    Change-Id: I5a38a0de76a78b28a205a8d19f2374830054e1ab
    Signed-off-by: melanie witt <melwittt@gmail.com>

--- nova/virt/libvirt/driver.py
+++ nova/virt/libvirt/driver.py
@@ -8174,6 +8174,28 @@ class LibvirtDriver(driver.ComputeDriver):
         finally:
             self._create_domain_cleanup_lxc(instance)
 
+    def _create_secret_for_vtpm(
+        self,
+        context: nova_context.RequestContext,
+        instance: 'objects.Instance',
+    ) -> ty.Tuple[ty.Any, ty.Optional[str]]:
+        secret_uuid, passphrase = crypto.ensure_vtpm_secret(context, instance)
+        secret_security = instance.system_metadata.get(
+            'image_hw_tpm_secret_security')
+
+        kwargs = {}
+        if secret_security == 'host':
+            # create_secret() already contains logic to default to the most
+            # secure ephemeral and private for TPM, so just specify if we
+            # don't want that.
+            kwargs = {'ephemeral': False, 'private': False}
+
+        libvirt_secret = self._host.create_secret(
+            'vtpm', instance.uuid, password=passphrase, uuid=secret_uuid,
+            **kwargs)
+
+        return libvirt_secret, secret_security
+
     def _create_guest(
         self,
         context: nova_context.RequestContext,
@@ -8191,15 +8213,13 @@ class LibvirtDriver(driver.ComputeDriver):
         :returns guest.Guest: Created guest.
         """
         libvirt_secret = None
+        secret_security = None
         # determine whether vTPM is in use and, if so, create the secret
         if CONF.libvirt.swtpm_enabled and hardware.get_vtpm_constraint(
             instance.flavor, instance.image_meta,
         ):
-            secret_uuid, passphrase = crypto.ensure_vtpm_secret(
+            libvirt_secret, secret_security = self._create_secret_for_vtpm(
                 context, instance)
-            libvirt_secret = self._host.create_secret(
-                'vtpm', instance.uuid, password=passphrase,
-                uuid=secret_uuid)
 
         try:
             guest = libvirt_guest.Guest.create(xml, self._host)
@@ -8212,7 +8232,7 @@ class LibvirtDriver(driver.ComputeDriver):
 
             return guest
         finally:
-            if libvirt_secret is not None:
+            if libvirt_secret is not None and secret_security != 'host':
                 libvirt_secret.undefine()
 
     def _neutron_failed_callback(self, event_name, instance):
