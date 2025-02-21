f6167aac9335b2ac46854c0d81ecbd6504983a2f
Artom Lifshitz <alifshit@redhat.com>
2025-02-21 12:10:28 -0500

    TPM: migrate legacy instances to new security policy
    For legacy instances that don't have a TPM secret security policy in
    their image properties or extra specs, start using init_host() to
    persist a TPM secret security policy in the system_metadata. If nothing
    is found in the image or flavor, use the configuration option. Record
    `provisional_tpm_secret_security = $policy` to indicate that the user
    has not yet accepted the new security policy. The user is expected to
    view the security policy chosen for them by the operator using the new
    2.98 microversion, and if they agree with the policy, hard reboot their
    instance for it to take effect. Subsequent patches will actually do the
    work of confirming the policy upon hard reboot. If the user disagrees,
    they can either never touch their instance again, or just delete it.
    
    The new _set_tpm_secret_security() method takes an argument that
    will be used in future patches:
    
    * confirmed tells it whether to persist the security policy as
      confirmed by the user. For now this can only be False as we're only
      migrating legacy instances and want the user to confirm in all
      cases, but future patches that add support for booting new instances
      where the security policy is confirmed implicitly by the user will
      have it True.
    
    To enable all of this, two new config options are added:
    [libvirt]default_tpm_secret_security and
    [libvirt]supported_tpm_secret_security.
    
    The first option specifies which TPM secret security policy is applied
    if nothing is set in the instance's flavor or image properties. The
    default is current behavior aka `user`.
    
    NOTE(artom) The spec put this in the compute/DEFAULT section. This
    patches changes that to [libvirt] to align with the other TPM options,
    and because libvirt is the only driver that supports vTPM.
    
    The second option specifies which TPM security policies the host
    supports. The possible values are `user`, `host` and `deployment`,
    and anything from none to all three can be listed (though none would
    be weird as it effectively disables TPM). Each value has a
    corresponding trait reported by the libvirt driver, and will be used
    in subsequent patches by a scheduler prefilter to select based on
    image properties or flavor extra specs.
    
    NOTE(artom) The spec put this in the compute/DEFAULT section. This
    patches changes that to [libvirt] to align with the other TPM options,
    and because libvirt is the only driver that supports vTPM.
    
    Related to blueprint vtpm-live-migration
    
    Change-Id: I40507397704f885e00e0bfa021a673ee2d5789a3
    Signed-off-by: melanie witt <melwittt@gmail.com>

--- nova/compute/manager.py
+++ nova/compute/manager.py
@@ -1021,8 +1021,7 @@ class ComputeManager(manager.Manager):
                     instance=instance)
 
     def _validate_vtpm_configuration(self, instances):
-        if self.driver.capabilities.get('supports_vtpm', False):
-            return
+        supports_vtpm = self.driver.capabilities.get('supports_vtpm', False)
 
         for instance in instances:
             if instance.deleted:
@@ -1030,15 +1029,71 @@ class ComputeManager(manager.Manager):
 
             # NOTE(stephenfin): We don't have an attribute on the instance to
             # check for this, so we need to inspect the flavor/image metadata
-            if hardware.get_vtpm_constraint(
-                instance.flavor, instance.image_meta,
-            ):
+            requested_vtpm = hardware.get_vtpm_constraint(instance.flavor,
+                                                          instance.image_meta)
+            if not supports_vtpm and requested_vtpm:
                 msg = _(
                     'This host has instances with the vTPM feature enabled, '
                     'but the host is not correctly configured; enable '
                     'vTPM support.'
                 )
                 raise exception.InvalidConfiguration(msg)
+
+            # For existing instances, set TPM security provisionally. The
+            # security policy will not take effect until the user confirms it
+            # by hard rebooting the instance.
+            if self._set_tpm_secret_security(instance, confirmed=False):
+                instance.save()
+
+    def _set_tpm_secret_security(
+            self, instance: 'objects.Instance', confirmed: bool) -> bool:
+        """Set TPM secret security attributes on an instance.
+
+        If the instance has a TPM device, set a TPM secret security policy,
+        either by the admin in the flavor or the user in the image. If nothing
+        is set, apply the default from this host's configuration.
+
+        NOTE: The caller is responsible for saving the instance afterwards if
+        desired.
+
+        :param instance: The instance object.
+        :param confirmed: Whether the TPM secret security policy has been
+            confirmed by the user. For new instances this should be True. For
+            legacy instances being migrated to the new way of doing things,
+            this should be False and the security policy will be considered
+            provisional until the user confirms it with a hard reboot.
+
+        :returns: True if TPM security metadata was updated, False otherwise
+        """
+        # Check if the instance has a TPM from the tpm_version and tpm_model
+        # extra specs or image properties. If the instance has no TPM, we don't
+        # need to do anything.
+        if not hardware.get_vtpm_constraint(instance.flavor,
+                                            instance.image_meta):
+            return False
+
+        # If the instance already has a secret security policy set, there is
+        # nothing to do.
+        if ('image_hw_tpm_secret_security' in instance.system_metadata or
+                'provisional_tpm_secret_security' in instance.system_metadata):
+            return False
+
+        # If the instance has a TPM, check if a secret security policy has
+        # been specified from the tpm_secret_security the extra spec or
+        # image property.
+        security = hardware.get_tpm_secret_security_constraint(
+            instance.flavor, instance.image_meta)
+        # If one was not specified, take the configured default.
+        security = (
+            security or CONF.libvirt.default_tpm_secret_security)
+        # Then set it in the instance system metadata.
+        if confirmed:
+            updates = {'image_hw_tpm_secret_security': security}
+        else:
+            updates = {'provisional_tpm_secret_security': security}
+
+        instance.system_metadata.update(updates)
+        return True
 
     def _reset_live_migration(self, context, instance):
         migration = None
@@ -1657,7 +1712,8 @@ class ComputeManager(manager.Manager):
 
         instances = objects.InstanceList.get_by_host(
             context, self.host,
-            expected_attrs=['info_cache', 'metadata', 'numa_topology'])
+            expected_attrs=['info_cache', 'metadata', 'numa_topology',
+                            'system_metadata'])
 
         self.init_virt_events()
 
--- nova/tests/unit/compute/test_compute_mgr.py
+++ nova/tests/unit/compute/test_compute_mgr.py
@@ -1042,7 +1042,10 @@ class ComputeManagerUnitTestCase(test.NoDBTestCase,
                           '_validate_vtpm_configuration')
         @mock.patch.object(manager.ComputeManager, '_init_instance')
         @mock.patch.object(self.compute, '_update_scheduler_instance_info')
-        def _do_mock_calls(mock_update_scheduler, mock_inst_init,
+        @mock.patch.object(manager.ComputeManager,
+                           '_set_tpm_secret_security', return_value=False)
+        def _do_mock_calls(mock_set_tpm_secret_security,
+                           mock_update_scheduler, mock_inst_init,
                            mock_validate_vtpm, mock_validate_pinning,
                            mock_destroy, mock_admin_ctxt, mock_host_get,
                            mock_init_host,
@@ -1071,7 +1074,8 @@ class ComputeManagerUnitTestCase(test.NoDBTestCase,
 
             mock_init_host.assert_called_once_with(host=our_host)
             mock_host_get.assert_called_once_with(self.context, our_host,
-                expected_attrs=['info_cache', 'metadata', 'numa_topology'])
+                expected_attrs=['info_cache', 'metadata', 'numa_topology',
+                                'system_metadata'])
 
             mock_update_scheduler.assert_called_once_with(
                 self.context, inst_list)
@@ -1234,7 +1238,8 @@ class ComputeManagerUnitTestCase(test.NoDBTestCase,
 
         mock_init_host.assert_called_once_with(host=our_host)
         mock_host_get.assert_called_once_with(self.context, our_host,
-            expected_attrs=['info_cache', 'metadata', 'numa_topology'])
+            expected_attrs=['info_cache', 'metadata', 'numa_topology',
+                            'system_metadata'])
         mock_init_virt.assert_called_once_with()
         mock_temp_mut.assert_called_once_with(self.context, read_deleted='yes')
         mock_get_inst.assert_called_once_with(self.context)
@@ -1695,6 +1700,7 @@ class ComputeManagerUnitTestCase(test.NoDBTestCase,
         image_meta = objects.ImageMeta.from_dict({})
 
         instance_2.flavor.extra_specs = {'hw:tpm_version': '2.0'}
+        instance_2.system_metadata = {}
 
         instance_3.deleted = True
 
@@ -1724,11 +1730,15 @@ class ComputeManagerUnitTestCase(test.NoDBTestCase,
             'host is not correctly configured; ',
             str(ex))
 
-    def test__validate_vtpm_configuration_supported(self):
+    @mock.patch('nova.objects.Instance.save')
+    def test__validate_vtpm_configuration_supported(self, mock_save):
         """Test that the entire check is skipped if the driver supports
         vTPM.
         """
         self._test__validate_vtpm_configuration(supports_vtpm=True)
+        # One instance should be saved after the TPM secret security policy is
+        # set in the system metadata.
+        mock_save.assert_called_once_with()
 
     def test__get_power_state_InstanceNotFound(self):
         instance = fake_instance.fake_instance_obj(
