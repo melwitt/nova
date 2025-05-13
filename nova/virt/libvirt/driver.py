30519027ecce792074dfe9a3f20217ad28b25ddc
melanie witt <melwittt@gmail.com>
2025-05-12 18:25:54 -0700

    debug printing in libvirt driver
    Change-Id: I59993e1b7d691b5d63c5111efaa9f51dbaf61f20

--- nova/virt/libvirt/driver.py
+++ nova/virt/libvirt/driver.py
@@ -8319,6 +8319,7 @@ class LibvirtDriver(driver.ComputeDriver):
         ):
             libvirt_secret, secret_security = self._create_secret_for_vtpm(
                 context, instance)
+            print(f'secret_security = {secret_security}')
 
         try:
             guest = libvirt_guest.Guest.create(xml, self._host)
