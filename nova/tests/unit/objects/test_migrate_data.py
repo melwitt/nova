e4f1a7882000a1407939144a60041713e02c30da
melanie witt <melwittt@gmail.com>
2025-06-12 20:00:55 +0000

    Add vtpm_secret_(uuid|value) to LibvirtLiveMigrateData
    This is needed in order to pass TPM secret information to the
    destination over RPC to support the 'host' secret security mode.
    
    The fields are nullable so that secret security modes 'user' and
    'deployment' may set them to None.
    
    A setting of None lets the other security modes convey that they are
    actively choosing not to pass any data in the vTPM fields. This is
    important for interacting with older compute hosts in the middle of a
    rolling upgrade. We do not want to backlevel new LibvirtLiveMigrateData
    objects involving vTPM because older compute hosts cannot support vTPM
    live migration in any capacity.
    
    Related to blueprint vtpm-live-migration
    
    Change-Id: If2ff2a7bb41dea6e0959c965477b79f3f7d633e7
    Signed-off-by: melanie witt <melwittt@gmail.com>

--- nova/tests/unit/objects/test_migrate_data.py
+++ nova/tests/unit/objects/test_migrate_data.py
@@ -161,6 +161,18 @@ class _TestLibvirtLiveMigrateData(object):
         self.assertIn(
             'encryption_secret_uuid', primitive['nova_object.data'])
 
+    def test_vtpm_obj_make_compatible(self):
+        obj = migrate_data.LibvirtLiveMigrateData(
+            vtpm_secret_uuid=uuids.vtpm_secret,
+            vtpm_secret_value='password')
+        manifest = ovo_base.obj_tree_get_versions(obj.obj_name())
+        ex = self.assertRaises(
+            exception.ObjectActionError, obj.obj_to_primitive,
+            target_version='1.13', version_manifest=manifest)
+        self.assertIn(
+            'vtpm_secret_uuid and vtpm_secret_value are not supported in '
+            'version (1, 13)', str(ex))
+
     def test_vif_migrate_data(self):
         source_vif = network_model.VIF(
             id=uuids.port_id,
