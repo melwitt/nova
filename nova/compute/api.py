fa9444a3d240b8a8f425a8ab893b8bfb5bd23255
melanie witt <melwittt@gmail.com>
2025-08-18 21:03:14 +0000

    TPM: bump service version and require it for live migration
    This bumps the service version and adds a minimum service version check
    in the API to reject live migration requests for vTPM instances until
    the entire cloud is upgraded to the new version.
    
    Related to blueprint vtpm-live-migration
    
    Change-Id: I7daef8037385a4077dc0a78f03ae4b34a57560b7
    Signed-off-by: melanie witt <melwittt@gmail.com>

--- nova/compute/api.py
+++ nova/compute/api.py
@@ -127,6 +127,8 @@ SUPPORT_SHARES = 67
 MIN_COMPUTE_SOUND_MODEL_TRAITS = 69
 MIN_COMPUTE_USB_MODEL_TRAITS = 70
 
+MIN_COMPUTE_VTPM_LIVE_MIGRATION = 71
+
 # FIXME(danms): Keep a global cache of the cells we find the
 # first time we look. This needs to be refreshed on a timer or
 # trigger.
@@ -273,6 +275,10 @@ def reject_legacy_vtpm_live_migration(function):
         vtpm = instance.system_metadata.get('vtpm_secret_uuid')
         security = instance.system_metadata.get('image_hw_tpm_secret_security')
         if vtpm:
+            min_ver = objects.service.get_minimum_version_all_cells(
+                nova_context.get_admin_context(), ['nova-compute'])
+            if min_ver < MIN_COMPUTE_VTPM_LIVE_MIGRATION:
+                raise exception.VTPMOldCompute()
             if security != 'host':
                 raise exception.OperationNotSupportedForVTPM(
                     instance_uuid=instance.uuid,
@@ -5629,6 +5635,13 @@ class API:
             # This is essentially a hint to the scheduler to only consider
             # the specified host but still run it through the filters.
             request_spec.requested_destination = destination
+
+        if (instance.system_metadata.get('vtpm_secret_uuid') and
+                force is True and host_name):
+            target = nodes[0]
+            service = objects.Service.get_by_id(context, target.service_id)
+            if service.version < MIN_COMPUTE_VTPM_LIVE_MIGRATION:
+                raise exception.VTPMOldCompute(host=target.host)
 
         try:
             self.compute_task_api.live_migrate_instance(context, instance,
